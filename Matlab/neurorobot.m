d6f8a6af6caa59df75808c9829d5691af156ec73
Christopher Harris <christopher@vertebot.com>
2019-04-08 06:34:18 -0400

    Last updates for Lesson 3

--- Matlab/neurorobot.m
+++ Matlab/neurorobot.m
@@ -8,9 +8,9 @@
 
 %% Settings
 rak_only = 0;
-camera_present = 0;
+camera_present = 1;
 use_webcam = 0;
-bluetooth_present = 0;
+bluetooth_present = 1;
 bg_brain = 1;
 draw_synapse_strengths = 1;
 draw_neuron_numbers = 0;
