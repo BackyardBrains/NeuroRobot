ddc513817e1ed433b4895a196eb577dca6dcd80f
Christopher Harris <christopher@backyardbrains.com>
2019-04-18 17:24:10 -0400

    Fix

--- Matlab/startScriptRAK5206_edit.m
+++ Matlab/startScriptRAK5206_edit.m
@@ -5,7 +5,9 @@ clear
 
 
 %% MEX build
-mex RAK5206.cpp -IC:\boost_1_68_0 -LC:\boost_1_68_0\stage\lib -LC:\ffmpeg-4.1-win64-dev\lib -IC:\ffmpeg-4.1-win64-dev\include -lavcodec -lavformat -lavutil -lswscale -llibboost_system-vc141-mt-x64-1_68 -llibboost_chrono-vc141-mt-x64-1_68 -D_WIN32_WINNT=0x0A00
+if ~exist('RAK5206.mexw64', 'var')
+    mex RAK5206.cpp -IC:\boost_1_69_0 -LC:\boost_1_69_0\stage\lib -LC:\ffmpeg-4.1.1-win64-dev\lib -IC:\ffmpeg-4.1.1-win64-dev\include -lavcodec -lavformat -lavutil -lswscale -llibboost_system-vc141-mt-x64-1_69 -llibboost_chrono-vc141-mt-x64-1_69 -D_WIN32_WINNT=0x0A00
+end
 
 
 %% Create RAK object
@@ -21,15 +23,14 @@ serialArray = [];
 %% Prepare figure
 fig1 = figure(1);
 clf
-set(fig1, 'position', [1 41 1920 1003])
+% set(fig1, 'position', [1 41 1920 1003])
+set(fig1, 'position', [1 41 1536 800.8])
 set(fig1, 'NumberTitle', 'off', 'Name', 'Neurorobot Matlab C++ WiFi RAK interface')
 set(fig1, 'menubar', 'none', 'toolbar', 'none')
 vid_ax = axes('position', [0.05 0.15 0.9 0.8]);
 p1 = imshow(uint8(255* ones(720, 1280, 3)), []);
 button_stop = uicontrol('Style', 'pushbutton', 'String', 'Stop', 'units', 'normalized', 'position', [0.4 0.05 0.2 0.05]);
 set(button_stop, 'Callback', 'flag_run = 0;', 'FontSize', 18, 'FontName', 'Comic Book')
-text_serial_send = uicontrol('Style', 'text', 'String', 'Send', 'units', 'normalized', 'position', [0.1 0.05 0.1 0.05], 'FontName', 'Comic Book', 'fontsize', 18, 'horizontalalignment', 'center');
-text_serial_receive = uicontrol('Style', 'text', 'String', 'Receive', 'units', 'normalized', 'position', [0.65 0.05 0.3 0.05], 'FontName', 'Comic Book', 'fontsize', 18, 'horizontalalignment', 'center');
 
 
 %% Main loop
@@ -50,21 +51,21 @@ while rak.isRunning() && flag_run
     set(p1, 'CData', imageMat);
     drawnow
 
-    %% Read serial
-    serial_receive = rak.readSerial();
-    if ~isempty(serial_receive)
-        serialArray = [serialArray serial_receive];
-        a = regexp(serialArray, '[\r]');
-        l = length(serialArray);
-        if ~isempty(a) && l >= 6
-            a = a(end);
-            x = serialArray(l-5:l-1);       
-            disp('Received:')
-            disp(x)
-            serialArray = [];
-        end
-%         text_serial_receive.String = horzcat('Received: ', num2str(serial_receive));
-    end
+%     %% Read serial
+%     serial_receive = rak.readSerial();
+%     if ~isempty(serial_receive)
+%         serialArray = [serialArray serial_receive];
+%         a = regexp(serialArray, '[\r]');
+%         l = length(serialArray);
+%         if ~isempty(a) && l >= 6
+%             a = a(end);
+%             x = serialArray(l-5:l-1);       
+%             disp('Received:')
+%             disp(x)
+%             serialArray = [];
+%         end
+% %         text_serial_receive.String = horzcat('Received: ', num2str(serial_receive));
+%     end
 
     %% Motors
     if ~rem(serialCounter, 50)
@@ -80,7 +81,6 @@ while rak.isRunning() && flag_run
         rak.writeSerial(this_send)
         disp(horzcat('Step ', num2str(serialCounter)))
     end
-%     text_serial_send.String = horzcat('Sent: ', this_send);   
     
     %% Hearing
 %     audioMat = [audioMat rak.readAudio()];
