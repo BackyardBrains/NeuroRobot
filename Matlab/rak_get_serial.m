02d8ca59a3ae794eb3e16f577ac6058323c0c6ed
stanislavmircic <stanislavmircic@gmail.com>
2019-08-20 13:42:53 +0200

    Code displayes error if serial data exist but it's length is less than 24

--- Matlab/rak_get_serial.m
+++ Matlab/rak_get_serial.m
@@ -1,10 +1,15 @@
 serial_receive = rak_cam.readSerial();
 
 if ~isempty(serial_receive)
-    these_vals = str2num(serial_receive(1:24));
-    this_duration = these_vals(3);
-    this_distance = (this_duration/2) / 29.1;
-    this_distance(this_distance == 0) = 300;
+    if(length(serial_receive)>=24)    
+        these_vals = str2num(serial_receive(1:24));
+        this_duration = these_vals(3);
+        this_distance = (this_duration/2) / 29.1;
+        this_distance(this_distance == 0) = 300;
+    else
+        disp('rak_cam readSerial size less than required:')
+        disp(serial_receive)
+    end
 else
     this_distance = 300;
     disp('rak_cam readSerial empty')
