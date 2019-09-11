aa004a63d9a9c33a7ff4be9db3d8fd2c9146e462
stanislavmircic <stanislavmircic@gmail.com>
2019-09-11 14:40:00 +0200

    Only last serial data are returned from shared memmory

--- Matlab/rak_get_serial.m
+++ Matlab/rak_get_serial.m
@@ -1,7 +1,7 @@
 serial_receive = rak_cam.readSerial();
 
 if ~isempty(serial_receive)
-    if(length(serial_receive)>=24)    
+    if(length(serial_receive)>=24)   
         these_vals = str2num(serial_receive(1:24));
         this_duration = these_vals(3);
         this_distance = (this_duration/2) / 29.1;
