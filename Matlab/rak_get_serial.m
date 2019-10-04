02d8875332b4b0c73b0dd3d2e6a8e41463e68bc8
Christopher Harris <christopher@backyardbrains.com>
2019-10-04 10:31:10 -0400

    Update rak_get_serial.m

--- Matlab/rak_get_serial.m
+++ Matlab/rak_get_serial.m
@@ -3,9 +3,14 @@ serial_receive = rak_cam.readSerial();
 if ~isempty(serial_receive)
     if(length(serial_receive)>=24)   
         these_vals = str2num(serial_receive(1:24));
-        this_duration = these_vals(3);
-        this_distance = (this_duration/2) / 29.1;
-        this_distance(this_distance == 0) = 300;
+        if ~isempty(these_vals)
+            this_duration = these_vals(3);
+            this_distance = (this_duration/2) / 29.1;
+            this_distance(this_distance == 0) = 300;
+        else
+            disp('Empty serial values after str2num')
+            this_distance = 300;
+        end
     else
         disp('rak serial < 24 samples')
         this_distance = 300;
