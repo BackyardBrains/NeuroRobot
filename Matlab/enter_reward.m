d04d062624a7c9a8f3b022d0ee8a125ebe191ef2
Christopher Harris <christopher@vertebot.com>
2019-10-07 16:35:12 -0400

    One more thing

--- Matlab/enter_reward.m
+++ Matlab/enter_reward.m
@@ -9,21 +9,21 @@ if run_button == 5
         command_log.n = command_log.n + 1;
     end
 
-    % Capture reward image and audio
-    if isfield(brain, 'rewarded_frames')
-        nreward = size(brain.rewarded_frames, 4) + 1;
-    else
-        brain.rewarded_frames = zeros(720, 1280, 3, 'uint8');
-        brain.nrewarded_sounds = zeros(1000, 1);
-        nreward = 1;
-    end  
-    brain.rewarded_frames(:, :, :, nreward) = large_frame;
-    if isempty(this_audio)
-        this_audio = zeros(1, 1000);
-    elseif length(this_audio) > 1000
-        this_audio = this_audio(1, 1:1000);
-    end
-    brain.nrewarded_sounds(:, nreward) = this_audio; 
+%     % Capture reward image and audio
+%     if isfield(brain, 'rewarded_frames')
+%         nreward = size(brain.rewarded_frames, 4) + 1;
+%     else
+%         brain.rewarded_frames = zeros(720, 1280, 3, 'uint8');
+%         brain.nrewarded_sounds = zeros(1000, 1);
+%         nreward = 1;
+%     end  
+%     brain.rewarded_frames(:, :, :, nreward) = large_frame;
+%     if isempty(this_audio)
+%         this_audio = zeros(1, 1000);
+%     elseif length(this_audio) > 1000
+%         this_audio = this_audio(1, 1:1000);
+%     end
+%     brain.nrewarded_sounds(:, nreward) = this_audio; 
     
     % Display and update
     disp(horzcat('Dopamine reward :)'))
--- Matlab/population_realtime2.m
+++ Matlab/population_realtime2.m
@@ -2,10 +2,8 @@
 close all
 clear
 
-tic
-
-t1 = datetime(2019,10,4,9,30,00);
-t2 = datetime(2019,10,4,13,00,00);
+t1 = datetime(2019,10,4,11,10,00);
+t2 = datetime(2019,10,4,12,20,00);
 
 n = 0;
 a = [];
@@ -16,63 +14,30 @@ n_local_datas = size(local_database, 1);
 for n_local_data = 1:n_local_datas
     disp(horzcat(num2str(n_local_data), ' of ', num2str(n_local_datas)))
     load(strcat('.\Command\', local_database(n_local_data).name))
-    yyyy = str2double(local_database(n_local_data).name(1:4));
-    MM = str2double(local_database(n_local_data).name(6:7));
-    DD = str2double(local_database(n_local_data).name(9:10));
-    hh = str2double(local_database(n_local_data).name(12:13));
-    mm = str2double(local_database(n_local_data).name(15:16));
-    ss = str2double(local_database(n_local_data).name(18:19));
-    ms = str2double(local_database(n_local_data).name(21:22));
+    stop_time = char(command_log.stop_time);
+    yyyy = str2double(stop_time(1:4));
+    MM = str2double(stop_time(6:7));
+    DD = str2double(stop_time(9:10));
+    hh = str2double(stop_time(12:13));
+    mm = str2double(stop_time(15:16));
+    ss = str2double(stop_time(18:19));
+    ms = str2double(stop_time(21:22));
     t = datetime(yyyy, MM, DD, hh, mm, ss, ms);
     
-    if isbetween(t,t1,t2)
+    if isbetween(t,t1,t2) && isfield(command_log, 'stop_event')
         disp('During Friday lesson')
         n = n + 1;
-
-%         end_time_chars = char(data.stop_time);
-%         yyyy = str2double(end_time_chars(1:4));
-%         MM = str2double(end_time_chars(6:7));
-%         DD = str2double(end_time_chars(9:10));
-%         hh = str2double(end_time_chars(12:13));
-%         mm = str2double(end_time_chars(15:16));
-%         ss = str2double(end_time_chars(18:19));
-%         ms = str2double(end_time_chars(21:22));     
-%         tx = datetime(yyyy, MM, DD, hh, mm, ss, ms);
-
-%         disp(horzcat('lifetimes: ', num2str(n)))
-        
-%             firing = data.firing;
-%             nneurons = size(firing, 1);
-%             nsteps = size(firing, 2);
-%             plot(linspace(t, tx, nsteps), mean(firing))
-
-%         plot([t tx], [n n], 'linewidth', 5, 'color', 'k')
-%         hold on
-%         drawnow
-        
-        nentrys = command_log.n - 1;
-        for nentry = 1:nentrys
-            
-            entry_chars = char(command_log.entry(nentry).time);
-            yyyy = str2double(entry_chars(1:4));
-            MM = str2double(entry_chars(6:7));
-            DD = str2double(entry_chars(9:10));
-            hh = str2double(entry_chars(12:13));
-            mm = str2double(entry_chars(15:16));
-            ss = str2double(entry_chars(18:19));
-            ms = str2double(entry_chars(21:22));        
-            ty = datetime(yyyy, MM, DD, hh, mm, ss, ms);
-            
-            if strcmp(command_log.entry(nentry).action, 'stop button')
-                b = [b; ty 1];
-                disp('Stop button')
-            elseif strcmp(command_log.entry(nentry).action, 'rak fail')
-                b = [b; ty 0];
-                disp('RAK fail')
-            end
+        if strcmp(command_log.stop_event, 'stop button')
+            a = [a; t];
+            disp('Stop button')
+        elseif strcmp(command_log.stop_event, 'rak fail')
+            b = [b; t];
+            disp('RAK fail')
         end
-        
     end
 end
 
-toc
+scatter(a, ones(1,length(a)), 'filled', 'markerfacecolor', [0.1 0.8 0.1])
+hold on
+scatter(b, ones(1,length(b)) + 1, 'filled', 'markerfacecolor', [1 0.3 0.3])
+ylim([0 3])
