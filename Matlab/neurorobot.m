80f5480ad079b4e99124bb4b1973c51778419c92
Christopher Harris <christopher@vertebot.com>
2019-12-05 20:18:49 -0500

    .

--- Matlab/neurorobot.m
+++ Matlab/neurorobot.m
@@ -23,7 +23,8 @@ use_cnn = 0;
 use_rcnn = 0;
 
 %% Advanced settings
-pulse_period = 0.125; % in seconds
+% pulse_period = 0.125; % in seconds
+pulse_period = 0.2; % in seconds
 save_data_and_commands = 0; %%% 
 save_brain_jpg = 0;
 brain_gen = 0;
