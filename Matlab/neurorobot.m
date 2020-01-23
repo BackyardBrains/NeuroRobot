a4d1fefdd17b55b66d4e47295599cd56a116494b
Christopher Harris <christopher@vertebot.com>
2020-01-23 16:06:38 -0500

    Added brain Karl

--- Matlab/neurorobot.m
+++ Matlab/neurorobot.m
@@ -16,16 +16,16 @@ camera_present = 1;
 use_webcam = 0;
 bluetooth_present = 0;
 
-hd_camera = 1;
+hd_camera = 0;
 grey_background = 1;
 draw_synapses = 1;
 use_cnn = 0;
 use_rcnn = 0;
-nma = 1;
+nma = 0;
 
 %% Advanced settings
-pulse_period = 0.1; % in seconds
-% pulse_period = 0.125; % in seconds
+% pulse_period = 0.1; % in seconds
+pulse_period = 0.125; % in seconds
 % pulse_period = 0.2; % in seconds
 save_data_and_commands = 0; %%% 
 save_brain_jpg = 0;
