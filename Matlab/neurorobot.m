a5ee658bbb08f20abd224f192a75840e544c3575
Christopher Harris <christopher@vertebot.com>
2020-02-03 14:21:54 -0500

    Fixing Merlin's addiction

--- Matlab/neurorobot.m
+++ Matlab/neurorobot.m
@@ -23,8 +23,8 @@ use_rcnn = 0;
 
 %% Advanced settings
 % pulse_period = 0.07; % in seconds
-% pulse_period = 0.125; % in seconds
-pulse_period = 0.1; % in seconds
+pulse_period = 0.125; % in seconds
+% pulse_period = 0.1; % in seconds
 save_data_and_commands = 0; %%% 
 save_brain_jpg = 0;
 brain_gen = 0;
