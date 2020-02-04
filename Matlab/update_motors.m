033db05c6bffb57d250f217487e0ae9b489a4e00
Christopher Harris <christopher@backyardbrains.com>
2020-02-04 13:25:10 -0500

    Update update_motors.m

--- Matlab/update_motors.m
+++ Matlab/update_motors.m
@@ -1,4 +1,4 @@
-min_torque = 75;
+min_torque = 120;
 
 if bluetooth_present || rak_only
 
@@ -12,7 +12,7 @@ if bluetooth_present || rak_only
     left_dir = max([1 - sign(left_torque) 1]);
     left_torque = abs(left_torque);
     left_torque(left_torque > 250) = 250;
-    left_torque(left_torque < min_torque) = 0;
+    left_torque(left_torque > min_torque) = 120;
     motor_command(1,3) = left_torque;
     motor_command(1,4) = left_dir;    
 
@@ -20,7 +20,7 @@ if bluetooth_present || rak_only
     right_dir = max([1 - sign(right_torque) 1]);
     right_torque = abs(right_torque); 
     right_torque(right_torque > 250) = 250;
-    right_torque(right_torque < min_torque) = 0;    
+    right_torque(right_torque > min_torque) = 120;    
     motor_command(1,1) = right_torque;
     motor_command(1,2) = right_dir;
     
