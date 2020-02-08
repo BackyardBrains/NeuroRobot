eca61c9dc70dcd1e59cd7435f35f968677f742ba
Christopher Harris <christopher@backyardbrains.com>
2020-02-08 16:39:18 -0500

    Update rak_get_serial.m

--- Matlab/rak_get_serial.m
+++ Matlab/rak_get_serial.m
@@ -1,28 +1,24 @@
-try
-    serial_receive = rak_cam.readSerial();
+
+serial_receive = rak_cam.readSerial();
 %     disp(serial_receive)
-    % this may need to read all available somehow or sensory input will pile up   
-    if ~isempty(serial_receive)
-        if(length(serial_receive)>=24)   
-            these_vals = str2num(serial_receive(1:24));
-            if ~isempty(these_vals)
-                this_duration = these_vals(3);
-                this_distance = (this_duration/2) / 29.1;
-                this_distance(this_distance == 0) = 300;
-            else
-                disp('Empty serial values after str2num')
-                this_distance = 300;
-            end
-        else
-            disp('rak serial < 24 samples')
-            this_distance = 300;
-        end
-    else
-        this_distance = 300;
-        if ~rem(nstep, 40) 
-            disp('rak_cam readSerial empty (displaying 1 of 40 errors)')
-        end
+% this may need to read all available somehow or sensory input will pile up   
+if ~isempty(serial_receive)
+    these_vals = [];
+    if length(serial_receive) == 9
+        these_vals = str2num(serial_receive(1:6));
+    elseif length(serial_receive) >= 24   
+        these_vals = str2num(serial_receive(1:24));
     end
-catch
-    disp('Error reading serial')
+    if ~isempty(these_vals)
+        this_duration = these_vals(3);
+        % get encoder vals here
+        this_distance = (this_duration/2) / 29.1;
+        this_distance(this_distance == 0) = 300;
+    else
+        disp('serial data is available but rak_get_serial.m is not reading it right')
+        this_distance = 300;
+    end
+else
+    disp('serial_receive is empty')
+    this_distance = 300;
 end
