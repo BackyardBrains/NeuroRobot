562c687545152db38971371d74fa4cc558bd927e
Christopher Harris <christopher@backyardbrains.com>
2020-02-11 14:48:18 -0500

    Hot to trot

--- Matlab/rak_get_serial.m
+++ Matlab/rak_get_serial.m
@@ -1,20 +1,15 @@
 
 serial_receive = rak_cam.readSerial();
-%     disp(serial_receive)
-% this may need to read all available somehow or sensory input will pile up   
+serial_data = strsplit(serial_receive, ',');
 if ~isempty(serial_receive)
-    these_vals = [];
-    if length(serial_receive) >= 6
-        these_vals = str2num(serial_receive(1:6));
-        this_duration = these_vals(3);
-        % get encoder vals here
-        this_distance = (this_duration/2) / 29.1;
-        this_distance(this_distance == 0) = 300;
-    else
-        disp('serial_receive is not empty but rak_get_serial.m is not reading it right')
-        this_distance = 300;
+    try
+        this_distance = str2num(serial_data{3});
+        this_distance(this_distance == 0) = 4000;
+    catch
+        disp('serial_receive is not empty but rak_get_serial.m does not recognize the content')
+        this_distance = 4000;
     end
 else
     disp('serial_receive is empty')
-    this_distance = 300;
+    this_distance = 4000;
 end
