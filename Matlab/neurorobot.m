514d5e06abd2c926ff0c1d7c8242688c80089b70
Christopher Harris <christopher@vertebot.com>
2020-02-23 17:32:19 -0500

    Update neurorobot.m

--- Matlab/neurorobot.m
+++ Matlab/neurorobot.m
@@ -18,18 +18,18 @@ end
 
 
 %% Settings
-rak_only = 0;
+rak_only = 1;
 camera_present = 1;
-use_webcam = 1;
+use_webcam = 0;
 bluetooth_present = 0;
 
-hd_camera = 0;
+hd_camera = 1;
 grey_background = 1;
 use_cnn = 0;
 use_rcnn = 0;
 
 %% Advanced settings
-pulse_period = 0.08; % in seconds
+pulse_period = 0.1; % in seconds
 % pulse_period = 0.25; % in seconds
 % pulse_period = 0.125; % in seconds
 save_data_and_commands = 0; %%% 
