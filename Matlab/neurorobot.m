6c42f51f93e741684f80ae521c8865bf25a7a2e9
Christopher Harris <christopher@vertebot.com>
2020-06-07 14:23:14 -0400

    Update neurorobot.m

--- Matlab/neurorobot.m
+++ Matlab/neurorobot.m
@@ -15,8 +15,8 @@
 % No webcamera, use these settings: rak_only = 0, camera_present = 0, use_webcam = 0 and bluetooth_present = 0
 
 rak_only = 0;
-camera_present = 0;
-use_webcam = 0;
+camera_present = 1;
+use_webcam = 1;
 bluetooth_present = 0;
 hd_camera = 0;
 use_cnn = 0;
