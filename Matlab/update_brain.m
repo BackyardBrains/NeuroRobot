a9c5024132fc08d9efead0c933ea57169d409baa
Christopher Harris <christopher@backyardbrains.com>
2020-07-07 12:58:26 -0400

    Update update_brain.m

--- Matlab/update_brain.m
+++ Matlab/update_brain.m
@@ -15,8 +15,8 @@ if nneurons
     
     % Calculate distance sensor input current
     dist_I = zeros(nneurons, 1);
-    dist_I(dist_prefs == 1) = sigmoid(this_distance, 200, -0.8) * 50;
-    dist_I(dist_prefs == 2) = sigmoid(this_distance, 500, -0.8) * 50;
+    dist_I(dist_prefs == 1) = sigmoid(this_distance, 20, -0.8) * 50;
+    dist_I(dist_prefs == 2) = sigmoid(this_distance, 200, -0.8) * 50;
     dist_I(dist_prefs == 3) = sigmoid(this_distance, 1000, -0.8) * 50;
 
     % Calculate distance sensor input current
