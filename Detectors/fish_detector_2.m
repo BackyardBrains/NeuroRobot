6a514c1c581cb2e555668f52093fb7adf290945c
Christopher Harris <christopher@backyardbrains.com>
2021-06-14 15:52:23 -0400

    .

--- Detectors/fish_detector_2.m
+++ Detectors/fish_detector_2.m
@@ -1,7 +1,6 @@
 
 close all
 clear
-
 % shrink_video
 % videoLabeler
 % combine_gtruths
@@ -9,10 +8,8 @@ load hero_truth_51
 data = objectDetectorTrainingData(gTruth, 'SamplingFactor', 1);
 % data = combine(frames, boxes);
 % count = 0; for ii = 1:size(boxes.LabelData) count = count + size(boxes.LabelData{263}, 1); end count
-
 net = alexnet;
 options = trainingOptions('sgdm', 'MaxEpochs', 30, 'MiniBatchSize', 64, 'InitialLearnRate', 0.0001, 'executionenvironment', 'gpu', 'Plots', 'training-progress');
 trainedDetector = trainRCNNObjectDetector(data, net, options);
 save('trainedDetector', 'trainedDetector')
-
 % test_fish_detector
