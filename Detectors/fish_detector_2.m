e470dbf0ba272f34fe80e0a201421f61ccfe712d
Christopher Harris <christopher@vertebot.com>
2021-06-16 10:55:53 -0400

    .

--- Detectors/fish_detector_2.m
+++ Detectors/fish_detector_2.m
@@ -1,21 +1,20 @@
 
-% close all
-% clear
+close all
+clear
 
 % shrink_video
 % videoLabeler
 % combine_gtruths
 
-% load hero_truth_51
-% data = objectDetectorTrainingData(gTruth, 'SamplingFactor', 1, 'WriteLocation', '.\frames')' ;
-load('trainingData')
-data = trainingData;
+load x5_truth
+trainingData = objectDetectorTrainingData(gTruth, 'SamplingFactor', 1, 'WriteLocation', '.\frames')' ;
+% load('trainingData')
 
-dag_to_rcnn
-
+% dag_to_rcnn
+net = alexnet;
 options = trainingOptions('sgdm', 'Shuffle', 'every-epoch', 'MaxEpochs', 5, ...
-    'MiniBatchSize', 4, 'InitialLearnRate', 0.001, 'executionenvironment', 'gpu', 'L2Regularization', 0.001);
-trainedDetector = trainFastRCNNObjectDetector(data, lgraph, options);
+    'MiniBatchSize', 128, 'InitialLearnRate', 0.001, 'executionenvironment', 'gpu');
+trainedDetector = trainFastRCNNObjectDetector(trainingData, net, options);
 save('trainedDetector', 'trainedDetector')
 
 % test_fish_detector
