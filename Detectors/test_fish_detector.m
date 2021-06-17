879792e309394e5dc70ec1490d5137c47af9d90a
Christopher Harris <christopher@backyardbrains.com>
2021-06-17 12:57:33 -0400

    .

--- Detectors/test_fish_detector.m
+++ Detectors/test_fish_detector.m
@@ -1,11 +1,14 @@
 
-input_video_name = 'hero52_small.MP4';
+cmap = cool;
+
+input_video_name = 'hero55_small.MP4';
 filename = input_video_name;
 filename(strfind(input_video_name, '_')) = [];
 vidReader = VideoReader(input_video_name);
 
-vidWriter = VideoWriter('hero52_small_ai.mp4','MPEG-4');
-open(vidWriter)
+output_video_name = horzcat(detector_name(17:end), '---', filename);
+vidWrite = VideoWriter(output_video_name,'MPEG-4');
+open(vidWrite)
 
 fig1 = figure(1);
 clf
@@ -24,8 +27,7 @@ clear pl
 
 for nstep = 1:vidReader.NumFrames
     frame = read(vidReader, nstep);
-    [bbox, score, label] = detect(trainedDetector, frame, 'NumStrongestRegions', 500, ...
-        'threshold', 0, 'ExecutionEnvironment', 'gpu', 'MiniBatchSize', 128);
+    [bbox, score, label] = detect(trainedDetector, frame, 'NumStrongestRegions', 1000, 'threshold', qi, 'ExecutionEnvironment', 'gpu');
     [mscore, midx] = max(score);
     mbbox = bbox(midx, :);
     if ~isempty(mscore)
@@ -33,26 +35,25 @@ for nstep = 1:vidReader.NumFrames
     end
     disp(num2str(nstep))
     im1.CData = frame;
-    
-    if mscore > qi        
-        
-        x = bbox(score > qi,1) + bbox(score > qi,3)/2;
-        y = bbox(score > qi,2) + bbox(score > qi,4)/2;                
-        mx = mbbox(:,1) + mbbox(:,3)/2;
-        my = mbbox(:,2) + mbbox(:,4)/2;     
-        
-        nboxes = length(x);
-        prev_length = length(zi);
-        zi(1 + prev_length : prev_length + nboxes) = 10;
-        for nbox = 1:nboxes
-            pl(prev_length + nbox).plt = plot(x, y, 'linestyle', 'none', 'color', [0.94 0.2 0.07], 'marker', 'o', 'markersize', 5);        
+
+    x = bbox(score > qi,1) + bbox(score > qi,3)/2;
+    y = bbox(score > qi,2) + bbox(score > qi,4)/2;                
+    mx = mbbox(:,1) + mbbox(:,3)/2;
+    my = mbbox(:,2) + mbbox(:,4)/2;     
+
+    nboxes = length(x);
+    prev_length = length(zi);
+    zi(1 + prev_length : prev_length + nboxes) = 10;
+    for nbox = nboxes:-1:1
+        if score(nbox)
+%             pl(prev_length + nbox).plt = plot(x, y, 'linestyle', 'none', 'color', [0.94 0.2 0.07], 'marker', 'o', 'markersize', 5);        
+            pl(prev_length + nbox).plt = plot(x, y, 'linestyle', 'none', 'color', cmap(round(score(nbox)* 64), :), 'marker', 'o', 'markersize', 5);        
         end
-                        
+    end
+
 %         plot(mx, my, 'linestyle', 'none', 'color', [0.94 0.2 0.07], 'marker', '.', 'markersize', 20);
 %         tx1.String = horzcat('fish: ', num2str(round(mscore * 100)/100));
 %         tx1.Position = [mx + 10 my - 3 0];
-        
-    end
     
     for ii = 1:length(zi)
         zi(ii) = zi(ii) - 1;
@@ -61,13 +62,16 @@ for nstep = 1:vidReader.NumFrames
         end
     end    
     
-    ti1.String = horzcat(filename, ', nframe = ', num2str(nstep), ' of ', num2str(vidReader.NumFrames), ', mscore = ', num2str(round(mscore * 100)/100));
+    ti1.String = horzcat('Inference on ', filename, ', nframe = ', num2str(nstep), ' of ', num2str(vidReader.NumFrames), ', mscore = ', num2str(round(mscore * 100)/100));
     drawnow
     
     imx = getframe(fig1);
-    writeVideo(vidWriter, imx.cdata);
+    writeVideo(vidWrite, imx.cdata);
 
 end
 
-close(vidWriter)
+close(vidWrite)
+close(fig1)
+stop(cam)
+
 
