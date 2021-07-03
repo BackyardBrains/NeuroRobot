e2ec440ffc2892bbf444861240f4160cf1ddf02a
Christopher Harris <christopher@backyardbrains.com>
2021-07-03 12:52:36 -0400

    .

--- Detectors/shrink_video.m
+++ Detectors/shrink_video.m
@@ -1,21 +1,22 @@
 
+close all
+clear
+
 %% Settings
-this_size = [227 227];
-input_video_name = '.\videos\fishX1.MP4';
+input_video_name = 'C:\Users\chris\Downloads\fish1x.mp4';
 
 %% Create video reader
 vidRead = VideoReader(input_video_name);
 
 %% Create video writer object
-vidWrite = VideoWriter('.\videos\fishX1_small.mp4','MPEG-4');
+vidWrite = VideoWriter('C:\Users\chris\Downloads\fish1_small.mp4','MPEG-4');
 open(vidWrite)
 
 %% Record video
 for nstep = 1:vidRead.NumFrames
     frame = read(vidRead, nstep);
-    frame = frame(701:1900, 501:3200, :);
-%     frame = imresize(frame, this_size);
-    frame = imresize(frame, 0.2);
+    frame = frame(600:2000, 500:3200, :);
+    frame = imresize(frame, 0.17);
     writeVideo(vidWrite, frame);
     disp(horzcat('nframe = ', num2str(nstep), ' of ', num2str(vidRead.NumFrames)))
 end
