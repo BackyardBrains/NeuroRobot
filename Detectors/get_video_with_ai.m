d23e9cb052c2047123ba296857afa02002e77487
Christopher Harris <christopher@vertebot.com>
2021-07-06 12:25:46 -0400

    .

--- Detectors/get_video_with_ai.m
+++ Detectors/get_video_with_ai.m
@@ -3,21 +3,18 @@
 
 
 %% Settings
+smalltalk = 1;
 net_input_size = [227 227];
 fps = 10;
-raw_video_filename = 'classroom3.mp4';
+raw_video_filename = 'office21.mp4';
 cam_id = 1;
 qi = 0.5;
 
 %% Prompt
-prompt_a = horzcat('You are a helpful artificial intelligence, specifically a text completion ', ...
-    'engine similar to GPT-3. You are assisting at a Summer Fellowship exploring how embedded ', ...
-    'neural networks can be used in research and education.\n\nThe following is a conversation ', ...
-    'between you and ');
+prompt_a = horzcat('You are a friendly, helpful, insightful artificial intelligence. You are assisting at a ', ...
+    'Summer Fellowship in neuroscience and AI.\n\nThe following is a conversation between you and ');
 
-prompt_c = horzcat(' (The description of the Summer Project ends here.)\n\n--- The dialog begins here ---', ...
-    '\nHuman: Hello, who are you?\nAI: I am an artificial intelligence created by OpenAI and Backyard Brains. ', ...
-    'How can I help you today?');
+prompt_c = '\n(The description of the Summer Project ends here.)\n\n--- The dialog begins here ---';
 
 prompt = '';
 
@@ -68,21 +65,22 @@ set(fig1, 'NumberTitle', 'off', 'Name', 'Talking Head Classifier')
 set(fig1, 'menubar', 'none', 'toolbar', 'none')
 set(fig1, 'position', fig_pos)
 
-% text_title = uicontrol('Style', 'text', 'String', 'Talking Head Classifier', 'units', 'normalized', 'position', [0.05 0.9 0.9 0.05], ...
-%     'FontName', 'Comic Book', 'fontsize', 16, 'horizontalalignment', 'center');
-
-ax_frame = axes('position', [0.02 0.1 0.47 0.86]);
+ax_frame = axes('position', [0.02 0.2 0.47 0.76]);
 im = image(frame);
 set(gca, 'xtick', [], 'ytick', [])
 ti1 = title('Preparing...');
 hold on
 pl(1).plt = plot(-1, -1, 'linestyle', 'none', 'color', cmap(1, :), 'marker', 'o', 'markersize', 8, 'linewidth', 2);
 
-object_strs = {'ariyana', 'head', 'nour', 'sarah', 'wenbo'};
+
+dialog_box = uicontrol('Style', 'text', 'String', '', 'units', 'normalized', 'position', [0.02 0.1 0.47 0.08], ...
+    'FontName', 'Arial', 'fontsize', 11, 'horizontalalignment', 'left');
+
+object_strs = {'Ariyana', 'Human (prob. Chris)', 'Nour', 'Sarah', 'Wenbo'};
 
 nobjects = size(object_strs, 2);
 object_scores = zeros(nobjects,1);
-ax_bar = axes('position', [0.55 0.15 0.4 0.78]);
+ax_bar = axes('position', [0.55 0.25 0.4 0.68]);
 object_bars = bar(object_scores);
 hold on
 ylabel('Confidence')
@@ -109,27 +107,33 @@ clear pl
 nframe = 0;
 record_flag = 0;
 caption_accepted = 0;
+time_steps = nan(1000,4);
+talk_steps = nan(1000,6);
 
 while ~stop_flag
     
     %% Update frame
+    tic
     nframe = nframe + 1;
     trigger(cam)
     frame = getdata(cam, 1);
     frame = frame(:, 281:1000, :);
     frame = imresize(frame, net_input_size);
     im.CData = frame;
+    time_steps(nframe, 1) = toc;
     
     %% Run inference
+    tic
     [bbox, score, label] = detect(trainedDetector, frame, 'NumStrongestRegions', 500, ...
         'threshold', 0, 'ExecutionEnvironment', 'gpu', 'MiniBatchSize', 128);
+    time_steps(nframe, 2) = toc;
     
-    %% Process and display network output
+    %% Process and network output
+    tic
     [mscore, midx] = max(score);
     mbbox = bbox(midx, :);
     mlabel = char(label(midx));
     
-    tic
     for nobject = 1:5
         if ~isempty(max(score(label == object_strs{nobject})))
             object_scores(nobject) = max(score(label == object_strs{nobject}));
@@ -145,6 +149,8 @@ while ~stop_flag
     prev_length = length(zi);
     zi(1 + prev_length : prev_length + nboxes) = 10;
     
+    
+    %% Display network output
     for nbox = nboxes:-1:1
         if mscore > qi
             axes(ax_frame)
@@ -160,14 +166,17 @@ while ~stop_flag
     end
     
     object_bars.YData = object_scores;
+    time_steps(nframe, 3) = toc;
     
     %% IF THE TALK BUTTON IS PRESSED
     if talk_flag
         
         %% Start recording audio
+        tic
         talk_flag = 0;
         record_flag = 30;
         record(recObj)
+        talk_steps(nframe, 1) = toc;
         
     end
     
@@ -190,32 +199,45 @@ while ~stop_flag
         record_flag = 0;
         
         %% Stop audio recording
+        tic
         ti1.String = 'Processing recorded audio...';
         drawnow
         stop(recObj)
         data = getaudiodata(recObj);
+        talk_steps(nframe, 2) = toc;
         
         %% Speech to text
+        tic
         ti1.String = 'Converting speech to text...';
         drawnow
         tableOut = speech2text(speechObject, data, 16000);
         cellOut = table2cell(tableOut(:,1));
         human_says = cellOut{1};
         human_says = char(human_says);
+        talk_steps(nframe, 3) = toc;
         
         %% IF HUMAN SPEECH/TEXT WAS FOUND
         if ~strcmp(human_says, 'NoResult')
             
             %% Echo
-            ti1.String = 'Echo...';
-            vocalize_this(horzcat('I heard you say: ', human_says))
-            disp(horzcat('Human says: ', human_says))
+            dialog_box.String = horzcat('Human: ', human_says);
+            if ~smalltalk
+                tic
+                ti1.String = 'Echo...';
+                
+                vocalize_this(horzcat('I heard you say: ', human_says))
+                disp(horzcat('Human says: ', human_says))
             
-            button_talk.String = 'Accept';
-            set(button_talk, 'Callback', 'caption_accepted = 1;')
             
-            button_stop.String = 'Reject';
-            set(button_stop, 'Callback', 'caption_accepted = 2;')
+                button_talk.String = 'Accept';
+                set(button_talk, 'Callback', 'caption_accepted = 1;')
+
+                button_stop.String = 'Reject';
+                set(button_stop, 'Callback', 'caption_accepted = 2;')
+                talk_steps(nframe, 4) = toc;
+            else
+                caption_accepted = 1;
+            end
             
         else
             disp('No speech detected')
@@ -234,24 +256,24 @@ while ~stop_flag
             [i, this_person] = max(object_scores);
             if this_person == 1
                 this_person = 'Ariyana';
-                prompt_b = horzcat(', a Fellow at the Summer Course. This is a short description of her Summer Project: ');
+                prompt_b = ', a Fellow at the Summer Course.\nThis is a short description of her Summer Project:\n';
                 summer_project = 'Humans blink an average of 28,800 times a day. That means that a person who lives to 80 blinks around 840,960,000 times in their lifetime. The average blink lasts around 0.1 - 0.15 seconds, meaning that humans spend 126,144,000 seconds of their awake time on Earth with their eyes closed; that’s 973 - 1,460 days; 2.7 - 4 years! What’s happening in all that time? Are we simply doomed to miss out on those years of our life? Worry no more, FOMO glasses are (hopefully) here to help! These high tech and stylish glasses made to be wearable by the public will use Electrooculography (EOG) signals to detect blinks from the wearer and take photos in real time of all the life being missed out on. Say goodbye to the FOMO from that concert you had to blink through, and worry no more about missing frames from your favorite movie you claim to have seen the entirety of. Now with FOMO glasses, you can live your life to the fullest, and be present for every moment of it.';
             elseif this_person == 2
-                this_person = 'Dr. Harris';
-                prompt_b = ', your AI maintenance guy. This is a short description of his Summer Project: ';
-                summer_project = 'Cook meat. Summer Fellowship. Grants. Stop aging. Treat the missus.';
+                this_person = 'Chris';
+                prompt_b = ', your AI maintenance guy.\nThis is a short description of his Summer Project:\n';
+                summer_project = 'Cook meat. Help with the Summer Fellowship. Get grants. Stop aging. Treat the missus.';
             elseif this_person == 3
                 this_person = 'Nour';
-                prompt_b = horzcat(', a Fellow at the Summer Course. This is a short description of her Summer Project: ');
+                prompt_b = ', a Fellow at the Summer Course.\nThis is a short description of her Summer Project:\n';
                 summer_project = 'In my project, I will develop a small touch screen to show a series of playing cards while measuring EEGs. The subject chooses a card and AI determines which one it was. I will be training a machine learning algorithm to recognize the steady state visual evoked potentials (SSVEP) and/or the P300 surprise signal to give a guess on the run.';
             elseif this_person == 4
                 this_person = 'Sarah';
-                prompt_b = horzcat(', a Fellow at the Summer Course. This is a short description of her Summer Project: ');
+                prompt_b = horzcat(', a Fellow at the Summer Course.\nThis is a short description of her Summer Project:\n');
                 summer_project = 'Hi everyone! I am working on a lie detection device using TinyML that will hopefully be multimodal. I am currently working on assembling a base model using skin galvanic response before I move forward into considering EEG and other modalities.';
             elseif this_person == 5
                 this_person = 'Wenbo';
-                prompt_b = ', an Engineer at Backyard Brains. This is a short description of his Summer Project: ';
-                summer_project = horzcat('Engineering. Engineering. Engineering. Car. Treat the missus.');
+                prompt_b = ', an Engineer at Backyard Brains.\nThis is a short description of his Summer Project:\n';
+                summer_project = 'Engineering. Engineering. Engineering. Car. Treat the missus.';
             end
             
             prompt = horzcat(prompt_a, this_person, prompt_b, summer_project, prompt_c);
@@ -266,6 +288,7 @@ while ~stop_flag
         %             py.importlib.reload(m);
         
         %% Text completion / AI response
+        tic
         prompt = append(prompt, '\nAI:');
         py_str = py.ai.gpt3(prompt);
         ai_says = strtrim(char(py_str));
@@ -273,14 +296,19 @@ while ~stop_flag
         if ~isempty(linebreaks)
             ai_says(linebreaks(1):end) = [];
         end
-        human_says = 'NoResult';
         ti1.String = 'Vocalizing...';
+        talk_steps(nframe, 5) = toc;
+        
+        tic
+        dialog_box.String = horzcat('Human: ', human_says, newline, 'AI: ', ai_says);
+        human_says = 'NoResult';
         vocalize_this(ai_says)
         disp(horzcat('AI says: ', ai_says))
         
         ai_says = horzcat(' ', ai_says);
         prompt = append(prompt, ai_says);
         nappends = nappends + 1;
+        talk_steps(nframe, 6) = toc;
         
     end
     
@@ -294,8 +322,10 @@ while ~stop_flag
         set(button_stop, 'Callback', 'stop_flag = 1;')
     end
     
+    tic
     drawnow
     writeVideo(vidWrite, frame);
+    time_steps(nframe, 4) = toc;
     
 end
 
