8832b92a758c89b0b6b8ccb4abce4562ec43f75b
Christopher Harris <christopher@vertebot.com>
2021-09-22 11:45:07 -0400

    .

--- NeuroRobotToolbox/process_visual_input.m
+++ NeuroRobotToolbox/process_visual_input.m
@@ -1,6 +1,4 @@
 
-% I2 = illumgray(large_frame, 5);
-
 for ncam = 1:2
 
     if ncam == 1
@@ -10,11 +8,6 @@ for ncam = 1:2
         uframe = imresize(right_eye_frame, net_input_size);
         frame = single(uframe);
     end
-
-%     if sum(I2)
-%         frame = chromadapt(frame, I2, 'ColorSpace', 'linear-rgb');
-%         frame = lin2rgb(frame);    
-%     end
 
     % Red
     red = frame(:,:,1) > frame(:,:,2) * 1.8 & frame(:,:,1) > frame(:,:,3) * 1.8;
@@ -28,10 +21,8 @@ for ncam = 1:2
         this_score = sigmoid(npx, 1000, 0.01) * 50;
         if ncam == 1
             temporal_score = sigmoid(((227 - mean(x)) / 227), 0.95, 5) * this_score;
-%             temporal_score = ((227 - mean(x)) / 227) * this_score;
         elseif ncam == 2
             temporal_score = sigmoid((mean(x) / 227), 0.95, 5) * this_score;
-%             temporal_score = (mean(x) / 227) * this_score;
         end        
     else
         this_score = 0;
@@ -94,8 +85,6 @@ for ncam = 1:2
     elseif use_rcnn
         try
             aitic = tic;
-%             [bbox, score] = detect(rcnn, frame, 'NumStrongestRegions', 100, 'MiniBatchSize', 32, 'ExecutionEnvironment', 'gpu');
-%             [bbox, score] = detect(trainedDetector, uframe, 'NumStrongestRegions', 100, 'threshold', 0.3, 'ExecutionEnvironment', 'gpu');
             
             [bbox, score, label] = detect(trainedDetector, frame, 'NumStrongestRegions', 500, 'threshold', 0, 'ExecutionEnvironment', 'gpu', 'MiniBatchSize', 128);
             [mscore, midx] = max(score);
@@ -112,42 +101,8 @@ for ncam = 1:2
                 vis_pref_vals(nobject + 6, ncam) = cnn_out;
             end
             
-            
-            
-            
-            
-%             if isempty(bbox)
-%                 score = 0;
-%             end
-%             if length(score) > 1
-%                 [score, idx] = max(score);
-%                 bbox = bbox(idx, :);
-%             end
-
-%             object_scores
-                
-            
-            
-            
-%             if ~isempty(bbox)
-%                 if ncam == 1
-%                     this_val = ((227 - (bbox(1) + (bbox(3) / 2))) / 227);
-%                     temporal_cnn_out = cnn_out * sigmoid(this_val, 0.7, 5);
-%                 elseif ncam == 2
-%                     this_val = ((bbox(1) + (bbox(3) / 2)) / 227);
-%                     temporal_cnn_out = cnn_out * sigmoid(this_val, 0.7, 5);
-%                 end
-%             else
-%                 temporal_cnn_out = 0;
-%                 bbox = [0 0 0 0];
-%             end
-%             vis_pref_vals(8, ncam) = temporal_cnn_out;
-%             
-%             vis_pref_vals(9, ncam) = cnn_out * ((bbox(3) * bbox(4)) > 11000);
-%             this_text = horzcat('score = ', num2str(round(score * 100)/100), ', cnn out = ', num2str(round(cnn_out)), ', step time = ', num2str(round(toc(aitic) * 1000)), ' ms');
-%             disp(this_text)
         catch
-            disp('process visual input break')
+            disp('visual processing rcnn break')
         end
     end
     
