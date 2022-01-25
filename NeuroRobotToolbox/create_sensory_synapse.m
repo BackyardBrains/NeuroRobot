368504198abd3f5e77a08e4ee8dc9002a3a11aa8
vertebot <christopher@backyardbrains.com>
2022-01-25 12:35:06 -0500

    .

--- NeuroRobotToolbox/create_sensory_synapse.m
+++ NeuroRobotToolbox/create_sensory_synapse.m
@@ -1,8 +1,10 @@
 
-disp('Left eye contact selected. Launching create_sensory_synapse...')
+disp('Eye contact selected. Launching create_sensory_synapse...')
 disp(horzcat('fig userdata = ', num2str(fig_design.UserData), ...
     ', presyn clear = ', num2str(~exist('presynaptic_neuron', 'var')), ...
     ', nneurons = ', num2str(nneurons)))
+
+synapse_found = 0;
 
 % If no other design action is in progress
 if fig_design.UserData == 0 && ~exist('presynaptic_neuron', 'var') && nneurons
@@ -23,18 +25,27 @@ if fig_design.UserData == 0 && ~exist('presynaptic_neuron', 'var') && nneurons
         xdist = contact_xys(ncontact, 1) - x;
         ydist = contact_xys(ncontact, 2) - y;  
         sdist = sqrt(xdist^2 + ydist^2);        
-        if sdist < 0.4
+        if sdist < 0.5
             presynaptic_contact = ncontact;
             contact_h(ncontact).MarkerFaceColor = sel_col_core;
             disp(horzcat('x = ', num2str(x), ', y = ', num2str(y), ...
                 ', sdist = ', num2str(sdist), ', ncontact = ', num2str(ncontact)))
+            synapse_found = synapse_found + 1;
         end
     end
     
-    % Text heading
-    text_heading = uicontrol('Style', 'text', 'String', 'Select a postsynaptic neuron', 'units', 'normalized', 'position', [0.02 0.92 0.29 0.06], 'backgroundcolor', fig_bg_col, 'fontsize', bfsize, 'horizontalalignment', 'left', 'fontname', gui_font_name, 'fontweight', gui_font_weight);
-
-    disp('End of create_sensory_synapse')
+    if synapse_found == 1
+        % Text heading
+        text_heading = uicontrol('Style', 'text', 'String', 'Select a postsynaptic neuron', 'units', 'normalized', 'position', [0.02 0.92 0.29 0.06], 'backgroundcolor', fig_bg_col, 'fontsize', bfsize, 'horizontalalignment', 'left', 'fontname', gui_font_name, 'fontweight', gui_font_weight);
+    elseif synapse_found > 1
+        error('Too many contacts identified. Reduce sdist.')
+    elseif ~synapse_found
+        disp('No sensory contact found')
+        fig_design.UserData = 0;
+        set(button_add_neuron, 'enable', 'on')
+        set(button_add_network, 'enable', 'on')
+        set(button_return_to_runtime, 'enable', 'on')           
+    end
 
 else
 
