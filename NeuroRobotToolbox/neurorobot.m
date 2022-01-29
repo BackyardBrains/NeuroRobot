182a1f367937c6117027531d5edfcbf880a58de5
vertebot <christopher@backyardbrains.com>
2022-01-29 16:29:21 -0500

    Update neurorobot.m

--- NeuroRobotToolbox/neurorobot.m
+++ NeuroRobotToolbox/neurorobot.m
@@ -16,7 +16,7 @@ matlab_speaker_ctrl = 0;    % Multi tone output
 audio_th = 1;               % Audio threshold (increase if sound spectrum looks too crowded)
 pulse_period = 0.1;         % Step time in seconds
 dev_mode = 0;               % Custom rak_pulse_code
-bg_colors = 0;              % Use neuron color to indicate network ID, and neuron flickering to indicate spikes
+bg_colors = 1;              % Use neuron color to indicate network ID, and neuron flickering to indicate spikes
 microcircuit = 0;           % Use smaller neurons and synapses, no neuron numbers
 cpg_integration = 1;        % Add New Neurons (0 = semi random, 1 = add previously designed brains as CPGs)
 night_vision = 0;           % Use histeq to enhance image contrast
