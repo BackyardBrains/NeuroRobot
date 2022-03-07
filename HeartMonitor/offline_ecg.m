792df4fa004f5d4e2d56da387faab4faf56dffc7
Christopher Harris <christopher@vertebot.com>
2022-03-07 11:39:03 -0500

    ecg

---
+++ HeartMonitor/offline_ecg.m
@@ -0,0 +1,44 @@
+
+% Script for offline identification of QRS timepoints in ECG data recorded
+% with the BYB Heart and Brain SpikerBox
+% By Christopher Harris
+
+
+%% Load data
+[data, fs] = audioread('BYB_Recording_2022-03-07_10.44.50.wav');
+nsteps = length(data(:,1));
+
+%% Get R waves
+spike_threshold = prctile(data(:,1), 98);
+ismax = find(islocalmax(data(:,1), 'MinProminence', 0.4));
+ismax(data(ismax, 1) < spike_threshold) = [];
+l_edge = 0.2 * fs;
+r_edge = 0.2 * fs;
+ismax(ismax < l_edge | ismax > nsteps-r_edge) = [];
+nwaves = length(ismax);
+
+%% Get Q and S waves
+qrs_times = zeros(nwaves, 4);
+qrs_times(:,1) = ismax;
+for ii = 1:nwaves
+    pre_win = data(ismax(ii)-l_edge : ismax(ii), 1);
+    [q_val, q_ind] = min(pre_win);
+    qrs_times(ii, 2) = (ismax(ii)-l_edge) + q_ind - 1;
+    post_win = data(ismax(ii) : ismax(ii)+r_edge, 1);
+    [s_val, s_ind] = min(post_win);
+    qrs_times(ii, 3) = ismax(ii) + s_ind - 1;
+    qrs = qrs_times(ii,3) - qrs_times(ii,2);
+    qrs_times(ii,4) = qrs;
+end
+
+%% Plot
+figure(1)
+clf
+set(1, 'position', [376 760 1045 179], 'color', 'w')
+plot(data(:,1), 'linestyle', '-', 'linewidth', 2, 'color', [0.2 0.2 0.2], 'DisplayName', 'ECG');
+hold on
+plot(qrs_times(:,1), data(qrs_times(:,1),1), 'marker', 'o', 'linestyle', 'none', 'markersize', 10, 'color', [0.8 0.4 0.2], 'DisplayName','R', 'linewidth', 2);
+plot(qrs_times(:,2), data(qrs_times(:,2),1), 'marker', 'o', 'linestyle', 'none', 'markersize', 10, 'color', [0.2 0.8 0.2], 'DisplayName','Q', 'linewidth', 2);
+plot(qrs_times(:,3), data(qrs_times(:,3),1), 'marker', 'o', 'linestyle', 'none', 'markersize', 10, 'color', [0.2 0.4 0.8], 'DisplayName','S', 'linewidth', 2);
+xlim([0 nsteps])
+
--- HeartMonitor/online_ecg.m
+++ HeartMonitor/online_ecg.m
@@ -1,37 +1,44 @@
 
+% Script for online identification of QRS timepoints in ECG data recorded
+% with the BYB Heart and Brain SpikerBox
+% By Christopher Harris
 
-%%
-spikerbox = arduino('COM8','Uno');
+
+%% Connect SpikerBox
+clear spikerbox
+spikerbox = arduino('COM17','Uno'); % Use Device Manager to find SpikerBox COM port
 
 %% Prepare
-clc
 duration_in_s = 10;
 step_time_in_s = 0.05;
 spike_threshold = 4;
-nsteps = duration_in_s * (1/step_time_in_s);
-data = zeros(nsteps, 6);
-
 fs = 1/step_time_in_s;
 l_edge = 0.2 * fs;
 r_edge = 0.4 * fs;
+nsteps = duration_in_s * (1/step_time_in_s);
+data = zeros(nsteps, 6);
+flag = 0;
 
 %% Prepare figure
 figure(1)
 clf
+set(1, 'position', [376 700 1045 239], 'color', 'w')
+ax = axes('position', [0.1 0.3 0.8 0.6]);
 plot_ecg = plot(data(:,1), 'linestyle', '-', 'linewidth', 2, 'color', [0.2 0.4 0.8]);
 hold on
-plot_q = plot([20 40 60 80], [0 0 0 0], 'marker', 'o', 'linestyle', 'none', 'markersize', 10, 'color', 'r');
-plot_r = plot([20 40 60 80], [0 0 0 0], 'marker', 'o', 'linestyle', 'none', 'markersize', 10, 'color', [0.1 0.8 0.1]);
-plot_s = plot([20 40 60 80], [0 0 0 0], 'marker', 'o', 'linestyle', 'none', 'markersize', 10, 'color', 'b');
+plot_q = plot([20 40 60 80], [0 0 0 0], 'marker', 'o', 'linestyle', 'none', 'markersize', 10, 'color', [0.2 0.8 0.2], 'DisplayName','Q', 'linewidth', 2);
+plot_r = plot([20 40 60 80], [0 0 0 0], 'marker', 'o', 'linestyle', 'none', 'markersize', 10, 'color', [0.8 0.4 0.2], 'DisplayName','R', 'linewidth', 2);
+plot_s = plot([20 40 60 80], [0 0 0 0], 'marker', 'o', 'linestyle', 'none', 'markersize', 10, 'color', [0.2 0.4 0.8], 'DisplayName','S', 'linewidth', 2);
 plot_front = plot([0 0], [0 5], 'color', 'k', 'linestyle', '-', 'linewidth', 2);
 xlim([1 nsteps])
-ylim([0 6])
+bstop = uicontrol('Style', 'pushbutton', 'String', 'Stop', 'units', 'normalized', 'position', [0.4 0.05 0.2 0.1]);
+set(bstop, 'Callback', 'flag = 1;')
 
-%%
+%% Record and process data
 nstep = 0;
 refractory = 0;
 tic
-while nstep <= (duration_in_s * (1/step_time_in_s))
+while nstep <= (duration_in_s * (1/step_time_in_s)) && ~flag
     
     nstep = nstep + 1;
     data(nstep, :) = 0;
