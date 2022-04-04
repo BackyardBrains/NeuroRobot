cac2555d7b0baff8f82c6fc363133de5f9a64862
vertebot <christopher@backyardbrains.com>
2022-04-03 20:49:28 -0400

    Update neurorobot.m

--- NeuroRobotToolbox/neurorobot.m
+++ NeuroRobotToolbox/neurorobot.m
@@ -2,9 +2,9 @@
 %%%%  NeuroRobot App by Backyard Brains  %%%%
 
 %% Settings 
-rak_only = 0;               % Use robot with RAK5206 or RAK5270
+rak_only = 1;               % Use robot with RAK5206 or RAK5270
 camera_present = 1;         % Use robot camera or webcamera
-use_webcam = 1;             % Use webcamera
+use_webcam = 0;             % Use webcamera
 hd_camera = 0;              % Use robot with RAK5270
 use_esp32 = 0;              % Use robot with ESP32-CAM
 use_cnn = 0;                % Use a convolutional neural network (Googlenet) for object recognition
@@ -14,8 +14,8 @@ supervocal = 0;             % Custom word output (text-to-speech - REQUIRES WIND
 matlab_audio_rec = 1;       % Use computer microphone to listen
 matlab_speaker_ctrl = 0;    % Multi tone output
 audio_th = 1;               % Audio threshold (increase if sound spectrum looks too crowded)
-pulse_period = 0.5;         % Step time in seconds
-dev_mode = 1;               % Run brainless_first_visual_line once & brainless persistantly in rak_pulse_code
+pulse_period = 0.1;         % Step time in seconds
+dev_mode = 0;               % Run brainless_first_visual_line once & brainless persistantly in rak_pulse_code
 bg_colors = 1;              % Use neuron color to indicate network ID, and neuron flickering to indicate spikes
 microcircuit = 0;           % Use smaller neurons and synapses, no neuron numbers
 cpg_integration = 1;        % Add New Neurons (0 = semi random, 1 = add previously designed brains as CPGs)
@@ -25,8 +25,8 @@ night_vision = 0;           % Use histeq to enhance image contrast
 brain_gen = 0;              % Use "Create New Brain" to algorithmically generate new brains
 
 % RL settings
-save_experiences = 2;       % 0 = no, 1 = only tuples, 2 = tuples and audiovisual
-raw_or_bag = 2;             % 1 = raw, 2 = bag
+save_experiences = 0;       % 0 = no, 1 = only tuples, 2 = tuples and audiovisual
+raw_or_bag = 1;             % 1 = raw, 2 = bag
 use_controllers = 0;        % Add deep net controllers as scripts
 
 
