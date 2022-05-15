98a683878af095accec12bd7600843a4f8cb6abe
vertebot <christopher@backyardbrains.com>
2022-05-14 21:49:21 -0400

    .

--- NeuroRobotToolbox/basal_ganglia_sleeps.m
+++ NeuroRobotToolbox/basal_ganglia_sleeps.m
@@ -170,18 +170,19 @@ critic = rlQValueFunction(qTable,obsInfo,actInfo); % Learn rate
 
 agent_opt = rlDQNAgentOptions;
 agent_opt.DiscountFactor = 0.95;
-agent_opt.EpsilonGreedyExploration.Epsilon = 0.9;
-agent_opt.EpsilonGreedyExploration.EpsilonMin = 0.001;
-agent_opt.EpsilonGreedyExploration.EpsilonDecay = 0.0005;
+agent_opt.EpsilonGreedyExploration.Epsilon = 0.1;
+agent_opt.EpsilonGreedyExploration.EpsilonMin = 0.01;
+agent_opt.EpsilonGreedyExploration.EpsilonDecay = 0.005;
 agent = rlDQNAgent(critic, agent_opt);
 
+%%
 training_opts = rlTrainingOptions;
-training_opts.MaxStepsPerEpisode = 50;
-training_opts.MaxEpisodes = 5000;
+training_opts.MaxStepsPerEpisode = 500;
+training_opts.MaxEpisodes = 50000;
 training_opts.StopTrainingCriteria = "AverageReward";
 training_opts.ScoreAveragingWindowLength = 50;
-training_opts.UseParallel = true;
-trainingStats = train(agent,env);
+training_opts.UseParallel = false;
+trainingStats = train(agent,env, training_opts);
 
 save('agent', 'agent')
 %% Test controller
