374a5487c40e9e629fc68357c7d69e7c3ee8789c
vertebot <christopher@backyardbrains.com>
2022-06-01 14:05:26 -0400

    Update find_entropy.m

--- NeuroRobotToolbox/find_entropy.m
+++ NeuroRobotToolbox/find_entropy.m
@@ -3,20 +3,13 @@
 % MemImages_tocall = indexImages(imgSet); 
 % testSet = imageSet('.\Experiences\Recording_7\');
 
-for ii = 1:1000
-    id = randsample(testSet.Count, 1);
+xdata = zeros(imgSet.Count, testSet.Count);
+for id = 1:testSet.Count
+    disp(num2str(id/imgSet.Count))
     large_frame = readimage(testSet, id);
-    [IDs,scores]=retrieveImages(large_frame,MemImages_tocall,'NumResults',Inf);
-    if ~isempty(IDs) && scores(1) > 0.2
-        closest_match=readimage(imgSet, IDs(1));
-        figure(1)
-        clf
-        subplot(2,1,1)
-        imshow(large_frame)
-        subplot(2,1,2)
-        imshow(closest_match)
-        title(horzcat(num2str(scores(1))))
-        pause
+    [IDs,scores]=retrieveImages(large_frame, MemImages_tocall, 'NumResults', Inf);
+    if ~isempty(IDs)
+        xdata(IDs, id) = scores;
     end
 end
 
