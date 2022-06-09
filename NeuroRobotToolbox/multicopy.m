f7c8e952372d5afd5eae27bfe85273caa76510f4
Christopher Harris <christopher@vertebot.com>
2022-06-09 15:05:33 -0400

    .

--- NeuroRobotToolbox/multicopy.m
+++ NeuroRobotToolbox/multicopy.m
@@ -1,6 +1,8 @@
 
-% copyfile '.\Experiences\Recording_2-4\*.png' '.\Experiences\Recording_2-9'
-copyfile '.\Experiences\Recording_6\*.png' '.\Experiences\Recording_2-9'
-copyfile '.\Experiences\Recording_7\*.png' '.\Experiences\Recording_2-9'
-% copyfile '.\Experiences\Recording_8\*.png' '.\Experiences\Recording_2-9'
-copyfile '.\Experiences\Recording_9\*.png' '.\Experiences\Recording_2-9'
+% movefile '.\Experiences\*.png' '.\Experiences\Recording_14\'
+% 
+% copyfile '.\Experiences\*.mat' '.\Experiences\Recording_14\'
+% 
+% movefile '.\Experiences\*.mat' '.\Transfer\'
+
+copyfile '.\Experiences\Recording_11\*.mat' '.\Transfer\'
