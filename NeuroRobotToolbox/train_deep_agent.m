2589d1aa5ea4317a598edf8c22b7c9dffef7bfd1
Christopher Harris <christopher@vertebot.com>
2022-06-16 13:14:12 -0400

    .

--- NeuroRobotToolbox/train_deep_agent.m
+++ NeuroRobotToolbox/train_deep_agent.m
@@ -1,19 +1,16 @@
 
 agent_opt = rlDQNAgentOptions;
-agent_opt.DiscountFactor = 0.99;
-agent_opt.EpsilonGreedyExploration.Epsilon = 1;
-agent_opt.EpsilonGreedyExploration.EpsilonMin = 0.1;
-agent_opt.EpsilonGreedyExploration.EpsilonDecay = 0.05;
-
+% agent_opt.DiscountFactor = 0.99;
+agent_opt.EpsilonGreedyExploration.Epsilon = 0.1;
+agent_opt.EpsilonGreedyExploration.EpsilonMin = 0.01;
+agent_opt.EpsilonGreedyExploration.EpsilonDecay = 0.005;
 agent = rlDQNAgent(critic, agent_opt);
-
 training_opts = rlTrainingOptions;
-training_opts.MaxEpisodes = 100;
-training_opts.MaxStepsPerEpisode = 500;
-training_opts.StopTrainingCriteria = "EpisodeCount";
-training_opts.ScoreAveragingWindowLength = 10;
+% training_opts.MaxEpisodes = 100;
+% training_opts.MaxStepsPerEpisode = 500;
+training_opts.StopTrainingCriteria = "AverageReward";
+training_opts.ScoreAveragingWindowLength = 10000;
 training_opts.UseParallel = 1;
-
 trainingStats_deep = train(agent, env, training_opts);
 
 figure(12)
