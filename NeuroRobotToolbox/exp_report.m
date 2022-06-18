9f0c7275c86c3805e75c37c191a51215b5e8379a
vertebot <christopher@backyardbrains.com>
2022-06-18 14:40:34 -0400

    .

--- NeuroRobotToolbox/exp_report.m
+++ NeuroRobotToolbox/exp_report.m
@@ -5,17 +5,17 @@
 save('rl_exp_data', 'rl_exp_data')
 
 
-% %% States
-% figure(11)
-% clf
-% histogram(rl_exp_data(:,1))
-% title('States observed during the experiment')
-% 
-% %% Actions
-% figure(12)
-% clf
-% histogram(rl_exp_data(:,2))
-% title('Actions performed during the experiment')
+%% States
+figure(11)
+clf
+histogram(rl_exp_data(:,1))
+title('States observed during the experiment')
+
+%% Actions
+figure(12)
+clf
+histogram(rl_exp_data(:,2))
+title('Actions performed during the experiment')
 
 %% Reward
 figure(13)
@@ -28,8 +28,10 @@ rl_r = rl_r - min(rl_r);
 rl_r = rl_r / max(rl_r);
 subplot(2,1,1)
 plot(rl_r)
+xlabel('Time')
+ylabel('Reward')
 subplot(2,1,2)
 histogram(rl_r)
-title('Rewards (1/dist) obtained during the experiment')
 xlabel('Reward amplitude')
 ylabel('Count')
+title('Rewards obtained during the experiment')
