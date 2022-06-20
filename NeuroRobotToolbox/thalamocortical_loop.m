504ece9cfb05edf77029faa48424c10ce03e4f78
vertebot <christopher@backyardbrains.com>
2022-06-20 16:01:03 -0400

    Update thalamocortical_loop.m

--- NeuroRobotToolbox/thalamocortical_loop.m
+++ NeuroRobotToolbox/thalamocortical_loop.m
@@ -7,22 +7,22 @@ if save_experiences > 0
     fname = strcat('.\Data\',this_time, '-', brain_name, '-', num2str(xstep), '-left_uframe.png');
     imwrite(left_uframe, fname);
     fname = strcat('.\Data\',this_time, '-', brain_name, '-', num2str(xstep), '-right_uframe.png');
-    imwrite(left_uframe, fname);    
+    imwrite(right_uframe, fname);    
     
     % Action
     torques = [left_torque_mem right_torque_mem];    
-    file_name = strcat('.\Data\', this_time, '-', brain_name, '-', num2str(xstep), '-torques.mat');
-    save(file_name, 'rl_action', '-mat')
+    fname = strcat('.\Data\', this_time, '-', brain_name, '-', num2str(xstep), '-torques.mat');
+    save(fname, 'rl_action', '-mat')
 
     % Non-state data
-    if save_experiences && (rak_only || use_esp32)
+    if rak_only || use_esp32
         fname = strcat('.\Data\', this_time, '-', brain_name, '-', num2str(xstep), '-serial_data.mat');
         save(fname, 'serial_data', '-mat');
     end
 
     % Audio
 %     if rak_only || use_esp32 || matlab_audio_rec
-%         fname = strcat('.\Experiences\', this_time, '-', brain_name, '-', num2str(xstep), '-this_audio.mat');
+%         fname = strcat('.\Data\', this_time, '-', brain_name, '-', num2str(xstep), '-this_audio.mat');
 %         save(fname, 'this_audio', '-mat');
 %     end
 
