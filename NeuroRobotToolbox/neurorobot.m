cec19f8fbfee3fe758b029cae2ad74878798503d
Christopher Harris <christopher@backyardbrains.com>
2022-06-22 09:39:58 -0400

    .

--- NeuroRobotToolbox/neurorobot.m
+++ NeuroRobotToolbox/neurorobot.m
@@ -2,11 +2,11 @@
 %%%%  NeuroRobot App by Backyard Brains  %%%%
 
 %% Settings 
-rak_only = 1;               % Use RAK5206 or RAK5270
+rak_only = 0;               % Use RAK5206 or RAK5270
 camera_present = 1;         % Use robot camera or webcamera
 use_webcam = 0;             % Use webcamera
 hd_camera = 0;              % Use robot with RAK5270
-use_esp32 = 0;              % Use robot with ESP32-CAM ---- you also need to add matlabwebsocket to path
+use_esp32 = 1;              % Use robot with ESP32-CAM ---- you also need to add matlabwebsocket to path
 use_cnn = 0;                % Use a convolutional neural network (Googlenet) for object recognition
 use_rcnn = 0;               % Use a convolution6al neural network (Alexnet) for custom object recognition (e.g. face detection)
 vocal = 0;                  % Custom sound output
@@ -14,7 +14,7 @@ supervocal = 0;             % Custom word output (text-to-speech - REQUIRES WIND
 matlab_audio_rec = 0;       % Use computer microphone to listen
 matlab_speaker_ctrl = 0;    % Multi tone output
 audio_th = 1;               % Audio threshold (increase if sound spectrum looks too crowded)
-pulse_period = 0.3;         % Step time in seconds
+pulse_period = 0.2;         % Step time in seconds
 dev_mode = 0;               % Run brainless_first_visual_line once & brainless persistantly in rak_pulse_code
 bg_colors = 1;              % Use neuron color to indicate network ID, and neuron flickering to indicate spikes
 microcircuit = 0;           % Use smaller neurons and synapses, no neuron numbers
@@ -126,6 +126,7 @@ vis_pref_names = {'Red', 'red-temp', 'Green', ...
     'green-temp', 'Blue', 'blue-temp', ...
     'Movement'};
 n_basic_vis_features = length(vis_pref_names);
+serial_data = [];
 
 if use_cnn
     labels = readcell('alllabels.txt');
