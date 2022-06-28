bdec7d1c84ebbdb7ef2e22b1133cb97816b1f9cf
vertebot <christopher@backyardbrains.com>
2022-06-28 19:37:39 -0400

    .

--- NeuroRobotToolbox/behavior_script_5.m
+++ NeuroRobotToolbox/behavior_script_5.m
@@ -2,23 +2,11 @@
 % Random walk RL
 
 script_step_count = script_step_count + 1;
-
-disp(strcat('xstep: ', num2str(xstep)))
-
-% left_state = classify(net, imresize(left_uframe, [50 50]));
-% disp(horzcat('left state: ', char(left_state)))
-% 
-% right_state = classify(net, imresize(right_uframe, [50 50]));
-% disp(horzcat('right state: ', char(right_state)))
-
 this_action = randsample(nactions, 1);
 this_motor_vector = motor_combs(this_action, :);
 
-% this_motor_vector = this_motor_vector + 25;
-% this_motor_vector = this_motor_vector / 2;
-
+disp(strcat('xstep: ', num2str(xstep)))
 disp(horzcat('action: ', num2str(this_action)))
-
 disp(horzcat('torques: ', num2str(this_motor_vector(1)), ' ', num2str(this_motor_vector(2))))
 
 if this_motor_vector(1) > 0
@@ -32,7 +20,3 @@ if this_motor_vector(2) > 0
 else
     right_backward = -this_motor_vector(2);
 end
-
-
-
-
