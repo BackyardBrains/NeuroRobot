d935054b1bfe0beb6f07f9bc0d948522a9c959c4
vertebot <christopher@backyardbrains.com>
2022-07-13 16:10:51 -0400

    .

--- NeuroRobotToolbox/integrate_ai.m
+++ NeuroRobotToolbox/integrate_ai.m
@@ -1,9 +1,7 @@
 
-% close all
 clear
 clc
 
-step_fig = 0;
 
 %% Ontology
 classifier_dir_name = '.\Data_1\Rec_2\';
@@ -48,20 +46,7 @@ disp(horzcat('n unique states: ', num2str(n_unique_states)))
 disp(horzcat('n uniqe actions: ', num2str(n_unique_actions)))
 disp(horzcat('n tuples: ', num2str(ntuples)))
 
-%% Prepare figure
-if step_fig
-    figure(1)
-    clf
-    subplot(1,2,1)
-    left_eye = image(zeros(227, 227, 3));
-    left_title = title('');
-    subplot(1,2,2)
-    right_eye = image(zeros(227, 227, 3));
-    right_title = title('');
-end
-
 %% 
-image_buffer = [];
 state_buffer = [];
 next_state_buffer = [];
 torque_buffer = [];
@@ -69,75 +54,30 @@ action_buffer = [];
 dist_buffer = [];
 atuples = zeros(ntuples - 1, 3);
 btuples = [];
-btorques = [];
 moving = 0;
 moving_counter = [];
-% xtuples = randsample(ntuples - 1, ntuples - 1);
-% ytuples = randsample(ntuples - 1, ntuples - 1);
-stepcount = 0;
 for ntuple = 1:ntuples - 1
 
-%     ntuple = xtuples(xtuple);
     if ~rem(ntuple, round((ntuples-1)/10))
         disp(num2str(ntuple/(ntuples-1)))
     end
 
     this_state = states(ntuple);
-    this_action = actions(ntuple);
     these_torques = torque_data(ntuple, :);
-    
-%     this_next_state = states(ytuples(ntuple) + 1);
-    this_next_state = states(ntuple + 1);
-    this_dist = dists(ntuple);
-    
-    str1 = image_dir(ntuple*2-1).name;
-    str1(end-15:end) = [];
-    str2 = image_dir(ntuple*2).name;
-    str2(end-16:end) = [];
-    str3 = torque_dir(ntuple).name;
-    str3(end-11:end) = [];
-
-    if ~strcmp(str1, str2)
-        disp(horzcat('str1: ', str1))
-        disp(horzcat('str2: ', str2))
-        error('image mismatch')
-    end
-    if ~strcmp(str1, str3)
-        disp(horzcat('str1: ', str1))
-        disp(horzcat('str3: ', str3))        
-        error('image/torque mismatch')
-    end
-
-    if step_fig
-        for ii = 1:2
-            this_ind = ntuple*2-(ii-1);
-            this_im = imread(strcat(image_dir(this_ind).folder, '\',  image_dir(this_ind).name));
-            if ii == 1
-                left_eye_frame = this_im;
-            elseif ii == 2
-                right_eye_frame = this_im;
-            end    
-        end
-%     image_buffer = [image_buffer; binocular];        
-    end
 
     state_buffer = [state_buffer; this_state];
-    next_state_buffer = [next_state_buffer; this_next_state];
     torque_buffer = [torque_buffer; these_torques];
-    action_buffer = [action_buffer; this_action];
-    dist_buffer = [dist_buffer; this_dist];
 
+    atuples(ntuple, 1) = this_state;
+    atuples(ntuple, 2) = states(ntuple + 1);
+    atuples(ntuple, 3) = actions(ntuple);    
     
-%     if sum(torques)
-    if this_next_state ~= this_state
-%     if stepcount < 10
-%         stepcount = stepcount + 1;
+%     if sum(these_torques)
+    if this_state ~= states(ntuple + 1)
         moving = moving + 1;
     elseif moving
-%     elseif stepcount
         moving_counter = [moving_counter moving];
         moving = 0;
-%         stepcount = 0;
         
         motor_vector = mean(torque_buffer(1:end-1, :), 1);
         motor_vector = padarray(motor_vector, [0 1], rand * 0.00001, 'pre');
@@ -145,45 +85,21 @@ for ntuple = 1:ntuples - 1
         r = corr(motor_vector', motor_combs');  
         [~, this_action] = max(r);
 
-        btuples = [btuples; state_buffer(1), next_state_buffer(end-1), this_action];
-        btorques = [btorques; motor_vector];
-        image_buffer = [];
-        state_buffer = [];
-        action_buffer = [];
-        dist_buffer = [];
-        torque_buffer = [];
-        next_state_buffer = [];
-    else
-        image_buffer = [];
-        state_buffer = [];
-        action_buffer = [];
-        torque_buffer = [];
-        next_state_buffer = [];
-        dist_buffer = [];
-    end
-    
-    atuples(ntuple, 1) = this_state;
-    atuples(ntuple, 2) = this_next_state;
-    atuples(ntuple, 3) = this_action;
+        btuples = [btuples; state_buffer(1), state_buffer(end), this_action];
 
-    if step_fig
-        left_eye.CData = right_eye_frame;
-        left_title.String = horzcat('S: ', char(unique_states(this_state)), ', A: ', num2str(this_action), ', Sx: ', char(unique_states(this_next_state)));
-        right_eye.CData = left_eye_frame;
-        right_title.String = horzcat('S: ', char(unique_states(this_state)), ', A: ', num2str(this_action), ', Sx: ', char(unique_states(this_next_state)));
-        clc
-        disp(horzcat('ntuple: ', num2str(ntuple), ' of ', num2str(ntuples)))
-        disp(horzcat('S: ', char(unique_states(this_state)), ', A: ', num2str(this_action), ', Sx: ', char(unique_states(this_next_state))));
-        disp(horzcat('S: ', num2str(this_state), ', A: ', num2str(this_action), ', Sx: ', num2str(this_next_state)))
-        drawnow
-        pause
+        state_buffer = [];
+        torque_buffer = [];
+
+    else
+        state_buffer = [];
+        torque_buffer = [];
     end
 end
 
 
 %% Get Markov Decision Process
-% tuples = atuples;
-tuples = btuples;
+tuples = atuples;
+% tuples = btuples;
 ntuples = size(tuples, 1);
 
 mdp = createMDP(n_unique_states, n_unique_actions);
@@ -260,7 +176,7 @@ export_fig(horzcat('mdp_', num2str(date)), '-r150', '-jpg', '-nocrop')
 %% Get reward
 reward_counter = zeros(size(mdp.R));
 reward_counter = reward_counter - 1;
-reward_counter(:,14,:) = 10;
+reward_counter(:,[1:4, 13:16],[1:2, 4:5]) = 10;
 mdp.R = reward_counter;
 disp(horzcat('total reward: ', num2str(sum(reward_counter(:)))))
 % mdp.TerminalStates = "s14";
--- NeuroRobotToolbox/integrate_ai2.m
+++ NeuroRobotToolbox/integrate_ai2.m
@@ -32,24 +32,24 @@ set(gca, 'xtick', [], 'ytick', [], 'xcolor', 'w', 'ycolor', 'w')
 export_fig(horzcat('agent1_', 'net'), '-r150', '-jpg', '-nocrop')
 save(horzcat('agent1'), 'agent')
 
-%% Agent 2 (Deep Q)
-agent_opt = rlDQNAgentOptions;
-agent_opt.DiscountFactor = 0.99;
-agent = rlDQNAgent(critic, agent_opt);
-training_opts = rlTrainingOptions;
-training_opts.MaxEpisodes = 200;
-training_opts.MaxStepsPerEpisode = 50;
-training_opts.StopTrainingValue = 500000;
-training_opts.StopTrainingCriteria = "AverageReward";
-training_opts.ScoreAveragingWindowLength = 5;
-training_opts.UseParallel = 1;
-trainingStats_deep = train(agent, env, training_opts);
-figure(12)
-clf
-set(gcf, 'color', 'w')
-scan_agent
-ylim([0 n_unique_states + 1])
-title('Agent 2')
-set(gca, 'xtick', [], 'ytick', [], 'xcolor', 'w', 'ycolor', 'w')
-export_fig(horzcat('agent2_', 'net'), '-r150', '-jpg', '-nocrop')
-save(horzcat('agent2'), 'agent')
+% %% Agent 2 (Deep Q)
+% agent_opt = rlDQNAgentOptions;
+% agent_opt.DiscountFactor = 0.99;
+% agent = rlDQNAgent(critic, agent_opt);
+% training_opts = rlTrainingOptions;
+% training_opts.MaxEpisodes = 200;
+% training_opts.MaxStepsPerEpisode = 50;
+% training_opts.StopTrainingValue = 500000;
+% training_opts.StopTrainingCriteria = "AverageReward";
+% training_opts.ScoreAveragingWindowLength = 5;
+% training_opts.UseParallel = 1;
+% trainingStats_deep = train(agent, env, training_opts);
+% figure(12)
+% clf
+% set(gcf, 'color', 'w')
+% scan_agent
+% ylim([0 n_unique_states + 1])
+% title('Agent 2')
+% set(gca, 'xtick', [], 'ytick', [], 'xcolor', 'w', 'ycolor', 'w')
+% export_fig(horzcat('agent2_', 'net'), '-r150', '-jpg', '-nocrop')
+% save(horzcat('agent2'), 'agent')
