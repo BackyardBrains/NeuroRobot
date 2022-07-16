b1f930c3ac7c304ea609238243863623929f1907
Christopher Harris <christopher@backyardbrains.com>
2022-07-15 20:09:58 -0400

    Update integrate_ai2.m

--- NeuroRobotToolbox/integrate_ai2.m
+++ NeuroRobotToolbox/integrate_ai2.m
@@ -2,7 +2,7 @@
 clear
 clc
 
-filename = 'rmdp';
+filename = 'mdp';
 load(filename)
 env = rlMDPEnv(mdp);
 
@@ -19,12 +19,12 @@ n_unique_actions = size(actInfo.Elements, 1);
 %% Agent 1 (Q)
 agent_opt = rlQAgentOptions;
 qOptions = rlOptimizerOptions;
-qOptions.LearnRate = 0.1;
+% qOptions.LearnRate = 0.1;
 agentOpts.CriticOptimizerOptions = qOptions;
 agent = rlQAgent(critic, agent_opt);
 training_opts = rlTrainingOptions;
-training_opts.MaxEpisodes = 100;
-training_opts.MaxStepsPerEpisode = 100;
+training_opts.MaxEpisodes = 500;
+training_opts.MaxStepsPerEpisode = 200;
 training_opts.StopTrainingValue = 10000;
 training_opts.StopTrainingCriteria = "AverageReward";
 training_opts.ScoreAveragingWindowLength = 100;
@@ -39,26 +39,26 @@ set(gca, 'xtick', [], 'ytick', [], 'xcolor', 'w', 'ycolor', 'w')
 export_fig(horzcat('agent1_', filename, '_net'), '-r150', '-jpg', '-nocrop')
 save(horzcat('agent1_', filename), 'agent')
 
-% 
-% %% Agent 2 (Deep Q)
-% agent_opt = rlDQNAgentOptions;
-% agent_opt.DiscountFactor = 0.99;
-% agent = rlDQNAgent(critic, agent_opt);
-% training_opts = rlTrainingOptions;
-% training_opts.MaxEpisodes = 500;
-% training_opts.MaxStepsPerEpisode = 50;
-% training_opts.StopTrainingValue = 10000;
-% training_opts.StopTrainingCriteria = "AverageReward";
-% training_opts.ScoreAveragingWindowLength = 50;
-% training_opts.UseParallel = 1;
-% trainingStats_deep = train(agent, env, training_opts);
-% figure(12)
-% clf
-% set(gcf, 'color', 'w')
-% scan_agent
-% ylim([0 n_unique_states + 1])
-% title(horzcat('Agent 2 - ', filename))
-% set(gca, 'xtick', [], 'ytick', [], 'xcolor', 'w', 'ycolor', 'w')
-% export_fig(horzcat('agent2_', filename, '_net'), '-r150', '-jpg', '-nocrop')
-% save(horzcat('agent2_', filename), 'agent')
-% 
+
+%% Agent 2 (Deep Q)
+agent_opt = rlDQNAgentOptions;
+agent_opt.DiscountFactor = 0.99;
+agent = rlDQNAgent(critic, agent_opt);
+training_opts = rlTrainingOptions;
+training_opts.MaxEpisodes = 500;
+training_opts.MaxStepsPerEpisode = 200;
+training_opts.StopTrainingValue = 10000;
+training_opts.StopTrainingCriteria = "AverageReward";
+training_opts.ScoreAveragingWindowLength = 50;
+training_opts.UseParallel = 1;
+trainingStats_deep = train(agent, env, training_opts);
+figure(12)
+clf
+set(gcf, 'color', 'w')
+scan_agent
+ylim([0 n_unique_states + 1])
+title(horzcat('Agent 2 - ', filename))
+set(gca, 'xtick', [], 'ytick', [], 'xcolor', 'w', 'ycolor', 'w')
+export_fig(horzcat('agent2_', filename, '_net'), '-r150', '-jpg', '-nocrop')
+save(horzcat('agent2_', filename), 'agent')
+
