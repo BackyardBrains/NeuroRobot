514294a0500bfb2e7905a1850abbd0fb2c8b8e72
vertebot <christopher@backyardbrains.com>
2022-07-17 15:48:37 -0400

    .

--- NeuroRobotToolbox/integrate_ai2.m
+++ NeuroRobotToolbox/integrate_ai2.m
@@ -2,7 +2,7 @@
 clear
 clc
 
-filename = 'rmdp';
+filename = 'mdp';
 load(filename)
 env = rlMDPEnv(mdp);
 
@@ -24,10 +24,10 @@ agentOpts.CriticOptimizerOptions = qOptions;
 agent = rlQAgent(critic, agent_opt);
 training_opts = rlTrainingOptions;
 training_opts.MaxEpisodes = 500;
-training_opts.MaxStepsPerEpisode = 200;
+training_opts.MaxStepsPerEpisode = 100;
 training_opts.StopTrainingValue = 10000;
 training_opts.StopTrainingCriteria = "AverageReward";
-training_opts.ScoreAveragingWindowLength = 100;
+training_opts.ScoreAveragingWindowLength = 50;
 trainingStats_shallow = train(agent,env, training_opts);
 figure(11)
 clf
@@ -40,25 +40,25 @@ export_fig(horzcat('agent1_', filename, '_net'), '-r150', '-jpg', '-nocrop')
 save(horzcat('agent1_', filename), 'agent')
 
 
-% %% Agent 2 (Deep Q)
-% agent_opt = rlDQNAgentOptions;
-% agent_opt.DiscountFactor = 0.99;
-% agent = rlDQNAgent(critic, agent_opt);
-% training_opts = rlTrainingOptions;
-% training_opts.MaxEpisodes = 200;
-% training_opts.MaxStepsPerEpisode = 50;
-% training_opts.StopTrainingValue = 10000;
-% training_opts.StopTrainingCriteria = "AverageReward";
-% training_opts.ScoreAveragingWindowLength = 100;
-% training_opts.UseParallel = 1;
-% trainingStats_deep = train(agent, env, training_opts);
-% figure(12)
-% clf
-% set(gcf, 'color', 'w')
-% scan_agent
-% ylim([0 n_unique_states + 1])
-% title(horzcat('Agent 2 - ', filename))
-% set(gca, 'xtick', [], 'ytick', [], 'xcolor', 'w', 'ycolor', 'w')
-% export_fig(horzcat('agent2_', filename, '_net'), '-r150', '-jpg', '-nocrop')
-% save(horzcat('agent2_', filename), 'agent')
+%% Agent 2 (Deep Q)
+agent_opt = rlDQNAgentOptions;
+agent_opt.DiscountFactor = 0.99;
+agent = rlDQNAgent(critic, agent_opt);
+training_opts = rlTrainingOptions;
+training_opts.MaxEpisodes = 500;
+training_opts.MaxStepsPerEpisode = 100;
+training_opts.StopTrainingValue = 10000;
+training_opts.StopTrainingCriteria = "AverageReward";
+training_opts.ScoreAveragingWindowLength = 50;
+training_opts.UseParallel = 1;
+trainingStats_deep = train(agent, env, training_opts);
+figure(12)
+clf
+set(gcf, 'color', 'w')
+scan_agent
+ylim([0 n_unique_states + 1])
+title(horzcat('Agent 2 - ', filename))
+set(gca, 'xtick', [], 'ytick', [], 'xcolor', 'w', 'ycolor', 'w')
+export_fig(horzcat('agent2_', filename, '_net'), '-r150', '-jpg', '-nocrop')
+save(horzcat('agent2_', filename), 'agent')
 
--- NeuroRobotToolbox/run_agent.m
+++ NeuroRobotToolbox/run_agent.m
@@ -18,20 +18,43 @@ if use_controllers
     end   
 
     disp(horzcat('state: ', num2str(this_state)))
+    
     this_action = getAction(agent, this_state);
-    this_motor_vector = motor_combs(cell2mat(this_action), :);
-    disp(horzcat('action: ', num2str(cell2mat(this_action)), ', torques: ', num2str(this_motor_vector(2:3))))
+
+    this_motor_vector = motor_combs(this_action, :);
+
+    disp(horzcat('action: ', num2str(cell2mat(this_action)), ', torques: ', num2str(this_motor_vector)))
+    
+    if this_motor_vector(1) > 0
+        left_forward = this_motor_vector(1);
+    else
+        left_backward = -this_motor_vector(1);
+    end
     
     if this_motor_vector(2) > 0
-        left_forward = this_motor_vector(2);
+        right_forward = this_motor_vector(2);
     else
-        left_backward = -this_motor_vector(2);
+        right_backward = -this_motor_vector(2);
     end
+
+
+%     this_motor_vector = motor_combs(cell2mat(this_action), :);
+%     this_motor_vector = this_motor_vector/2;
     
-    if this_motor_vector(3) > 0
-        right_forward = this_motor_vector(3);
-    else
-        right_backward = -this_motor_vector(3);
-    end
+%     disp(horzcat('action: ', num2str(cell2mat(this_action)), ', torques: ', num2str(this_motor_vector(2:3))))
+%     
+%     if this_motor_vector(2) > 0
+%         left_forward = this_motor_vector(2);
+%     else
+%         left_backward = -this_motor_vector(2);
+%     end
+%     
+%     if this_motor_vector(3) > 0
+%         right_forward = this_motor_vector(3);
+%     else
+%         right_backward = -this_motor_vector(3);
+%     end
+
+
 
 end
