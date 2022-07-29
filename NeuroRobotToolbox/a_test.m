3a1464a8293bf6c110f8367fce611c4bfa884cc2
vertebot <christopher@backyardbrains.com>
2022-07-29 15:45:14 -0400

    .

--- NeuroRobotToolbox/a_test.m
+++ NeuroRobotToolbox/a_test.m
@@ -4,37 +4,40 @@
 clear
 clc
 
-tuples_dir_name = 'C:\Users\Christopher Harris\RandomWalkData\';
+% tuples_dir_name = 'C:\Users\Christopher Harris\RandomWalkData\';
+tuples_dir_name = 'C:\Users\Christopher Harris\Rec_1\';
 image_ds = imageDatastore(tuples_dir_name, 'FileExtensions', '.png', 'IncludeSubfolders', true);
 imageIndex = indexImages(image_ds);
 imageIndex.MatchThreshold = 0;
 
-test_ind = 234;
-img = readimage(image_ds, test_ind);
-[inds,scores] = retrieveImages(img, imageIndex, 'NumResults', 10);
+% test_ind = 900;
+% img = readimage(image_ds, test_ind);
+% [inds,scores] = retrieveImages(img, imageIndex, 'NumResults', 10);
 
+%%
 nframes = length(imageIndex.ImageLocation);
 xdata = zeros(nframes, nframes);
 for ii = 1:nframes
     disp(horzcat('Processing image ', num2str(ii), ' of ', num2str(nframes)))
-    img = readimage(image_ds, test_ind);
-    uframe = memory_images{ii};
-    [inds,scores] = retrieveImages(uframe,imageIndex,'NumResults',Inf);
+    img = readimage(image_ds, ii);
+    [inds,scores] = retrieveImages(img, imageIndex, 'NumResults', Inf);
     xdata(ii, inds) = scores;
 end
 
-pdist
-T2 = clusterdata(X,'Maxclust',3); 
+%%
+n_unique_states = 100;
+[states, cstates] = kmeans(xdata, n_unique_states);
+figure(1)
+clf
+histogram(states, 'binwidth', 1)
 
-n_unique_actions = 10;
-[states, cstates] = kmeans(torque_data, n_unique_actions);
-save('actions2', 'actions')
-save('cactions', 'cactions')
-load('actions2')
-load('cactions')
-n_unique_actions = length(unique(actions));
-
-
-% n_unique_states
-
+for ii = 1:n_unique_states
+    figure(10+ii)
+    clf
+    x = find(states == ii);
+    if length(x) > 20
+        x = randsample(x, 20);
+    end
+    montage(imageIndex.ImageLocation(x))
+end
 
