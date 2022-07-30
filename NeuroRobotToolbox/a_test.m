a94e692f6d5e3f318e198b411508ca72aac56b90
vertebot <christopher@backyardbrains.com>
2022-07-30 13:29:32 -0400

    Update a_test.m

--- NeuroRobotToolbox/a_test.m
+++ NeuroRobotToolbox/a_test.m
@@ -6,9 +6,11 @@ tuples_dir_name = 'C:\Users\Christopher Harris\RandomWalkData\Rec_1\';
 image_ds = imageDatastore(tuples_dir_name, 'FileExtensions', '.png', 'IncludeSubfolders', true);
 imageIndex = indexImages(image_ds);
 imageIndex.MatchThreshold = 0;
+load('image_ds')
+load('imageIndex')
 
 %%
-nframes = length(imageIndex.ImageLocation);
+nframes = length(imageIndex.ImageLocation) / 2;
 xdata = zeros(nframes, nframes);
 for ii = 1:nframes
     disp(horzcat('Processing image ', num2str(ii), ' of ', num2str(nframes)))
@@ -17,8 +19,9 @@ for ii = 1:nframes
     xdata(ii, inds) = scores;
 end
 
+
 %%
-nclusters1 = 10;
+nclusters1 = 100;
 inds = kmeans(xdata, nclusters1);
 ydata = zeros(nclusters1, nframes);
 for ii = 1:nclusters1
@@ -29,23 +32,38 @@ end
 figure(1)
 clf
 histogram(inds, 'binwidth', 1)
+title('Pre merge')
+xlabel('State')
 
 %%
-noise_cluster = mode(inds);
+noise_cluster = mode(inds)
 noise_inds = yinds{noise_cluster};
+noise_fix = [];
+inds2 = inds;
 for ii = 1:length(noise_inds)
-    this_array = xdata(:, noise_inds(ii));
+    disp(num2str(ii/length(noise_inds)))
+    this_array = xdata(noise_inds(ii), :);
     temp = zeros(nclusters1, 1);
     for jj = 1:nclusters1
-        temp(jj) = immse(this_array, ydata(jj ,:)');
+        temp(jj) = immse(double(this_array), ydata(jj, :)');
     end
     [i, j] = sort(temp);
-    inds(noise_inds(ii)) = j(2);
+%     this_ind = randsample(2:10, 1);
+    this_ind = 2;
+    inds2(noise_inds(ii)) = j(this_ind);
+    noise_fix = [noise_fix j(this_ind)];
 end
 
 figure(2)
 clf
-histogram(inds, 'binwidth', 1)
+histogram(inds2, 'binwidth', 1)
+title('Post merge')
+xlabel('State')
+
+figure(3)
+clf
+histogram(noise_fix, 'binwidth', 1)
+title('Noise fix')
 
 %%
 for ii = 1:nclusters1
@@ -59,48 +77,37 @@ for ii = 1:nclusters1
 end
 
 %%
-inds = dbscan(xdata, 0.25, 5);
-unique_inds = unique(inds);
+inds3 = dbscan(xdata, 0.25, 5);
+unique_inds = unique(inds3);
 unique_inds(unique_inds == -1) = [];
 nclusters1 = length(unique_inds);
 disp(horzcat('nclusters: ', num2str(nclusters1)))
+inds4 = inds3;
 
 ydata = zeros(nclusters1, nframes);
 for ii = 1:nclusters1
-    ydata(ii, :) = mean(xdata(inds == ii, :));
-    yinds{ii} = find(inds == ii);
+    ydata(ii, :) = mean(xdata(inds3 == ii, :));
+    yinds{ii} = find(inds3 == ii);
 end
 
-figure(3)
+figure(5)
 clf
 subplot(1,2,1)
-histogram(inds, 'binwidth', 1)
+histogram(inds3, 'binwidth', 1)
 
 noise_inds = find(inds == -1);
 for ii = 1:length(noise_inds)
-    this_array = xdata(:, noise_inds(ii));
+    this_array = xdata(noise_inds(ii), :);
     temp = zeros(nclusters1, 1);
     for jj = 1:nclusters1
-        temp(jj) = immse(this_array, ydata(jj ,:)');
+        temp(jj) = immse(double(this_array), ydata(jj ,:)');
     end
     [i, j] = sort(temp);
-    inds(noise_inds(ii)) = j(2);
-end
-
-noise_inds = find(inds == mode(inds));
-for ii = 1:length(noise_inds)
-    this_array = xdata(:, noise_inds(ii));
-    temp = zeros(nclusters1, 1);
-    for jj = 1:nclusters1
-        temp(jj) = immse(this_array, ydata(jj ,:)');
-    end
-    [i, j] = sort(temp);
-    this_ind = randsample(2:round(nclusters1/2), 1);
-    inds(noise_inds(ii)) = j(this_ind);
+    inds4(noise_inds(ii)) = j(2);
 end
 
 subplot(1,2,2)
-histogram(inds, 'binwidth', 1)
+histogram(inds4, 'binwidth', 1)
 
 %%
 for ii = 1:nclusters1
