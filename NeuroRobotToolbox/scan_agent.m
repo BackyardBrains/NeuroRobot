50a03df5b5e7e700ec9fbd593aba7dd4b32f4cd9
Christopher Harris <christopher@backyardbrains.com>
2022-08-21 11:28:18 -0400

    .

--- NeuroRobotToolbox/scan_agent.m
+++ NeuroRobotToolbox/scan_agent.m
@@ -9,8 +9,10 @@ hold on
 for this_state = 1:n_unique_states
     this_action = getAction(agent, this_state);
     this_action = cell2mat(this_action);
+    plot([1 1], [this_state this_state], 'marker', '.', 'markersize', 50, 'color', [0.2 0.4 0.8])
+    plot([2 2], [this_action this_action] * (n_unique_states/n_unique_actions), 'marker', '.', 'markersize', 50, 'color', [0.2 0.4 0.8])
     plot([1 2], [this_state this_action * (n_unique_states/n_unique_actions)], 'linestyle', '-', 'marker', '.', 'markersize', 30, 'color', 'k', 'linewidth', 1);
-    if sum(this_state == reward_states)
+    if sum(this_state == reward_states) && this_action == mode(actions)
         plot([1 2], [this_state this_action * (n_unique_states/n_unique_actions)], 'linestyle', '-', 'marker', '.', 'markersize', 20, 'color', 'g', 'linewidth', 1);
     end
     drawnow
