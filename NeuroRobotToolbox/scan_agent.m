480405905e3e328b347b2d199609b03596c8493b
vertebot <christopher@backyardbrains.com>
2022-10-22 08:34:16 -0400

    Update scan_agent.m

--- NeuroRobotToolbox/scan_agent.m
+++ NeuroRobotToolbox/scan_agent.m
@@ -1,3 +1,6 @@
+figure(12)
+clf
+set(gcf, 'color', 'w')
 
 % n_unique_states  = 25;
 % n_unique_actions = 4;
@@ -17,6 +20,13 @@ for this_state = 1:n_unique_states
             plot([1 2], [this_state this_action * (n_unique_states/n_unique_actions)], 'linestyle', '-', 'marker', '.', 'markersize', 20, 'color', 'g', 'linewidth', 1);
         end
     end
+    mvec = motor_combs(this_action, :);
+    text(0.5, this_state, char(labels(this_state)))
+    text(1.5, this_action * (n_unique_states/n_unique_actions), strcat('A:', num2str(this_action), ...
+        'L:', num2str(mvec(this_action, 1), 'R:', num2str(mvec(this_action, 2)))))
     drawnow
 end
+ylim([0 n_unique_states + 1])
+xlim([0 3])
 
+set(gca, 'xtick', [], 'ytick', [], 'xcolor', 'w', 'ycolor', 'w')
