8e13fc82cd4275a486c93c01a7d92bb1e22add81
vertebot <christopher@backyardbrains.com>
2022-11-26 19:37:50 -0500

    .

--- NeuroRobotToolbox/draw_ext_view.m
+++ NeuroRobotToolbox/draw_ext_view.m
@@ -1,3 +1,10 @@
 figure(16)
+ax1 = axes('position', [0 0.05 1 0.9]);
 ext_im = image(ext_uframe);
+hold on
+ext_xy = plot(robot_xy(1), robot_xy(2), 'marker', '.', 'markersize', 20, 'color', 'r');
+set(ax1, 'xtick', [], 'ytick', [])
+
 ext_im.CData = ext_uframe;
+ext_xy.XData = robot_xy(1);
+ext_xy.YData = robot_xy(2);
--- NeuroRobotToolbox/thalamocortical_loop.m
+++ NeuroRobotToolbox/thalamocortical_loop.m
@@ -41,24 +41,26 @@ if save_experiences > 0
         ext_frame = getdata(ext_cam, 1); %%% <<<<< Commented out for packaging        
         ext_uframe = imresize(ext_frame, [227 404]);
         ext_im.CData = ext_uframe;
-        disp('33')
-
-%         ext_xframe = imsubtract(rgb2gray(ext_uframe), rgb2gray(prev_ext_uframe));
-%         ext_bwframe = ext_xframe > 2;  
-%         ext_blob = bwconncomp(ext_bwframe);
-%         if ext_blob.NumObjects
-%             [npx, this_blob] = max(cellfun(@numel,ext_blob.PixelIdxList));
-%             [y, x] = ind2sub(ext_blob.ImageSize, ext_blob.PixelIdxList{this_blob});
-%             robot_xy = [mean(x), mean(y)];
-%         else
+        ext_xframe = imsubtract(rgb2gray(ext_uframe), rgb2gray(prev_ext_uframe));
+        ext_bwframe = ext_xframe > 10;  
+        ext_blob = bwconncomp(ext_bwframe);
+        if ext_blob.NumObjects
+            [npx, this_blob] = max(cellfun(@numel,ext_blob.PixelIdxList));
+            [blob_y, blob_x] = ind2sub(ext_blob.ImageSize, ext_blob.PixelIdxList{this_blob});
+            robot_xy = [mean(blob_x), mean(blob_y)];
+        else
 %             robot_xy = [0 0];
-%         end
-%         prev_ext_uframe = ext_uframe;
-%         
-% %         fname = strcat(data_dir_name, rec_dir_name, '-', computer_name, '-', user_name, '-', this_time, '-', brain_name, '-', num2str(xstep), '-robot_xy.mat');
-%         fname = strcat(rec_dir_name_2, computer_name, '-', user_name, '-', this_time, '-', brain_name, '-', num2str(xstep), '-robot_xy.mat');        
-%         save(fname, 'robot_xy', '-mat');
-%         disp(horzcat('x = ', num2str(robot_xy(1)), ', y = ', num2str(robot_xy(2))))
+        end
+        prev_ext_uframe = ext_uframe;
+        
+%         fname = strcat(data_dir_name, rec_dir_name, '-', computer_name, '-', user_name, '-', this_time, '-', brain_name, '-', num2str(xstep), '-robot_xy.mat');
+        fname = strcat(rec_dir_name_2, computer_name, '-', user_name, '-', this_time, '-', brain_name, '-', num2str(xstep), '-robot_xy.mat');        
+        save(fname, 'robot_xy', '-mat');
+        disp(horzcat('x = ', num2str(robot_xy(1)), ', y = ', num2str(robot_xy(2))))
+
+        ext_im.CData = ext_uframe;
+        ext_xy.XData = robot_xy(1);
+        ext_xy.YData = robot_xy(2);
     end    
 
 end
