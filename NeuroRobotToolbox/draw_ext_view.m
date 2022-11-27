28410d886ce26bf947261fa78713e1cc9d0df40d
vertebot <christopher@backyardbrains.com>
2022-11-26 19:44:45 -0500

    .

--- NeuroRobotToolbox/draw_ext_view.m
+++ NeuroRobotToolbox/draw_ext_view.m
@@ -1,10 +1,10 @@
 figure(16)
 ax1 = axes('position', [0 0.05 1 0.9]);
-ext_im = image(ext_uframe);
+ext_im = image(zeros(720, 1280, 3, 'uint8'));
 hold on
-ext_xy = plot(robot_xy(1), robot_xy(2), 'marker', '.', 'markersize', 20, 'color', 'r');
+ext_xy = plot(0, 0, 'marker', '.', 'markersize', 20, 'color', 'r');
 set(ax1, 'xtick', [], 'ytick', [])
 
-ext_im.CData = ext_uframe;
-ext_xy.XData = robot_xy(1);
-ext_xy.YData = robot_xy(2);
+% ext_im.CData = ext_uframe;
+% ext_xy.XData = robot_xy(1);
+% ext_xy.YData = robot_xy(2);
