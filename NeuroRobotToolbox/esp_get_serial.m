2fd0aaaa73eaeb56eade0e74a432374b55999df6
Christopher Harris <christopher@backyardbrains.com>
2022-12-14 13:53:13 -0500

    .

--- NeuroRobotToolbox/esp_get_serial.m
+++ NeuroRobotToolbox/esp_get_serial.m
@@ -3,21 +3,19 @@ try
 catch
      disp('Cannot receive ESP32 serial')
 end
-% disp(serial_receive)
+
 if ~isempty(serial_receive)
-    
     serial_data = strsplit(serial_receive, ',');
-    try
-        
+    try       
         this_distance = str2double(serial_data{3});
         this_distance(this_distance == 0) = 4000;
+%         disp(num2str(this_distance))
        
     catch
         disp('serial_receive is not empty but ESP_get_serial.m does not recognize the content')
         this_distance = 4000;
     end
 else
-    
     disp('serial_receive is empty')
     this_distance = 4000;
 end
