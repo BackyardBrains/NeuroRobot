f76c2b274bdf87467f7f45e8ab697b7ad159bbc5
Christopher Harris <christopher@backyardbrains.com>
2023-02-10 23:14:35 -0500

    Update collapser.m

--- NeuroRobotToolbox/collapser.m
+++ NeuroRobotToolbox/collapser.m
@@ -24,28 +24,33 @@ ntuples = size(serial_dir, 1);
 disp(horzcat('ndists: ', num2str(ntuples)))
 
 %%
-for ntuple = 1:ntuples
+for ntuple = 40024:ntuples
 
     if ~rem(ntuple, round(ntuples/100))
         disp(horzcat('Counter: ', num2str(round(100*(ntuple/ntuples))), '%, ntuple: ', num2str(ntuple)))
     end
 
-    new_im = zeros(227, 404, 3, 'uint8');
+    new_im = zeros(100, 178, 3, 'uint8');
 
     this_ind = ntuple*2-1;    
-    left_im = imread(strcat(image_dir(this_ind).folder, '\',  image_dir(this_ind).name));
-%     left_im = imresize(left_im, [imdim imdim]);
+    left_im_link = strcat(image_dir(this_ind).folder, '\',  image_dir(this_ind).name);
+    left_im = imread(left_im_link);
+    left_im = imresize(left_im, [100 100]);
 
     this_ind = ntuple*2;
-    right_im = imread(strcat(image_dir(this_ind).folder, '\',  image_dir(this_ind).name));
-%     right_im = imresize(right_im, [imdim imdim]);
+    right_im_link = strcat(image_dir(this_ind).folder, '\',  image_dir(this_ind).name);
+    right_im = imread(right_im_link);
+    right_im = imresize(right_im, [100 100]);
 
-    new_im(:, 1:227, :) = left_im;
-    new_im(:, 178:404, :) = right_im;    
+%     new_im(:, 1:227, :) = left_im;
+%     new_im(:, 178:404, :) = right_im; 
+    new_im(:, 1:100, :) = left_im;
+    new_im(:, 79:178, :) = right_im; 
 
     this_name = serial_dir(ntuple).name;
     this_name(end-14:end) = [];
     fname = strcat(serial_dir(ntuple).folder, '\', this_name, num2str(ntuple), '_binoc.png');
     imwrite(new_im, fname);
-
+    delete(left_im_link)
+    delete(right_im_link)
 end
