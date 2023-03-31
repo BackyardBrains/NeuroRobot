e60c14bacca5b35ba63bcf777a465dc239ccead6
Christopher Harris <christopher@backyardbrains.com>
2023-03-30 22:30:03 -0400

    .

--- NeuroRobotToolbox/design_buttons.m
+++ NeuroRobotToolbox/design_buttons.m
@@ -11,21 +11,24 @@ button7_pos = [0.02 0.05 0.26 0.05];
 button_add_neuron = uicontrol('Style', 'pushbutton', 'String', 'Neuron', 'units', 'normalized', 'position', button1_pos, 'fontname', gui_font_name, 'fontweight', gui_font_weight);
 set(button_add_neuron, 'Callback', 'if fig_design.UserData == 0 button_add_neuron.BackgroundColor = [0.6 0.95 0.6]; neuron_or_network = 1; multi_neuron_opt = 0; end;', 'FontSize', bfsize + 4, 'BackgroundColor', [0.8 0.8 0.8])
     
-% Add Semi-random cluster
+% Add many neurons
 button_add_population = uicontrol('Style', 'pushbutton', 'String', 'Population', 'units', 'normalized', 'position', button2_pos, 'fontname', gui_font_name, 'fontweight', gui_font_weight);
 set(button_add_population, 'Callback', 'if fig_design.UserData == 0 button_add_population.BackgroundColor = [0.6 0.95 0.6]; neuron_or_network = 2; multi_neuron_opt = 1; end;', 'FontSize', bfsize + 4, 'BackgroundColor', [0.8 0.8 0.8])
 
-% Add Beautiful maths
+% Add neurons using maths
 button_add_algorithm = uicontrol('Style', 'pushbutton', 'String', 'Algorithm', 'units', 'normalized', 'position', button3_pos, 'fontname', gui_font_name, 'fontweight', gui_font_weight);
 set(button_add_algorithm, 'Callback', 'if fig_design.UserData == 0 button_add_algorithm.BackgroundColor = [0.6 0.95 0.6]; neuron_or_network = 2; multi_neuron_opt = 2; end;', 'FontSize', bfsize + 4, 'BackgroundColor', [0.8 0.8 0.8])
+set(button_add_algorithm, 'enable', 'off')
 
-% Add Agents
+% Add RL Agents
 button_add_agents = uicontrol('Style', 'pushbutton', 'String', 'Agent', 'units', 'normalized', 'position', button4_pos, 'fontname', gui_font_name, 'fontweight', gui_font_weight);
 set(button_add_agents, 'Callback', 'if fig_design.UserData == 0 button_add_agents.BackgroundColor = [0.6 0.95 0.6]; button_add_neuron.BackgroundColor = [0.8 0.8 0.8]; neuron_or_network = 2; multi_neuron_opt = 3; end;', 'FontSize', bfsize + 4, 'BackgroundColor', [0.8 0.8 0.8])
+set(button_add_agents, 'enable', 'off')
 
 % Brain 2 Circuit
 button_add_brain = uicontrol('Style', 'pushbutton', 'String', 'Brain', 'units', 'normalized', 'position', button5_pos, 'fontname', gui_font_name, 'fontweight', gui_font_weight);
 set(button_add_brain, 'Callback', 'if fig_design.UserData == 0 button_add_brain.BackgroundColor = [0.6 0.95 0.6]; button_add_neuron.BackgroundColor = [0.8 0.8 0.8]; neuron_or_network = 2; multi_neuron_opt = 4; end;', 'FontSize', bfsize + 4, 'BackgroundColor', [0.8 0.8 0.8])
+set(button_add_brain, 'enable', 'off')
 
 % Save brain
 button_save = uicontrol('Style', 'pushbutton', 'String', 'Save', 'units', 'normalized', 'position', button6_pos);
