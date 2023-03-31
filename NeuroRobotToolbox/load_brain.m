3286f4701ff4cb8944409eb8c89d64fd2858062f
Christopher Harris <christopher@backyardbrains.com>
2023-03-30 22:10:00 -0400

    Update load_brain.m

--- NeuroRobotToolbox/load_brain.m
+++ NeuroRobotToolbox/load_brain.m
@@ -38,14 +38,6 @@ nnetworks = length(unique(network_ids)); % There used to be a +1 hack here, remo
 network_drive = zeros(nnetworks, 3); 
 bg_neurons = brain.bg_neurons;
 
-try
-    vision_net_lock = brain.vision_net_lock;
-catch
-    vision_net_lock = '';
-end
-try
-    audio_out_lock = brain.audio_out_lock;
-catch
-    audio_out_lock = '';
-end
+vision_net_lock = brain.vision_net_lock;
+audio_out_lock = brain.audio_out_lock;
 
