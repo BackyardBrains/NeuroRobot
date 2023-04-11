c1e813ee0d85bd0f7397abe0bfda5f2517187129
Christopher Harris <christopher@backyardbrains.com>
2023-04-11 18:24:53 -0400

    Update ml_visualize_mdp.m

--- NeuroRobotToolbox/ml_visualize_mdp.m
+++ NeuroRobotToolbox/ml_visualize_mdp.m
@@ -8,12 +8,13 @@ fig_pos = get(0, 'screensize') + [0 49 0 -71];
 set(fig_ml, 'position', fig_pos, 'color', fig_bg_col) 
 
 data = zeros(n_unique_actions, 1);
-x = factor(n_unique_actions);
+x = ceil(sqrt(n_unique_actions));
+
 for naction = 1:n_unique_actions
 
     motor_out = round(mean(torque_data(actions == naction, :), 1));
     
-    subplot(x(1), x(2), naction)
+    subplot(x, x, naction)
     imagesc(mean(mdp.T(:,:,naction), 3), [0 1])
     title(horzcat('Action: ', num2str(naction), '.  L: ', num2str(motor_out(1)), ',  R: ', num2str(motor_out(2))))
     ylabel('State')
