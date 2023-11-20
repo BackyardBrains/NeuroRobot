6c52939fbb898c8a67ef53e5929b6dcd17e446c7
Christopher Harris <christopher@backyardbrains.com>
2023-11-20 11:31:44 -0500

    .

--- NeuroRobotToolbox/ml_chop.m
+++ NeuroRobotToolbox/ml_chop.m
@@ -1,7 +1,7 @@
 
 
 % this_lim = 3000;
-this_lim = 200000;
+this_lim = 150000;
 
 for nrec = 1:nrecs
     this_dir_name = strcat(dataset_dir_name, available_dirs(nrec).name);
@@ -13,13 +13,13 @@ for nrec = 1:nrecs
         this_new_dir_name = strcat(this_dir_name, 'Cut');
         mkdir(this_new_dir_name)
         for nfile = 1:this_lim
-            if ~rem(nfile, round(nfiles/10))
-                disp(horzcat('Process: ', num2str(round(100 * ((nfile)/nfiles))), ' %'))
+            if ~rem(nfile, round(nfiles/20))
+                disp(horzcat('Process: ', num2str(round(100 * ((nfile)/this_lim))), ' %'))
             end
             movefile(strcat(these_files(nfile).folder, '\', these_files(nfile).name), this_new_dir_name)            
         end
-        disp('Cut large dir in two')
+        disp(horzcat('Cut this large dir in two: ', available_dirs(nrec).name))
     else
-        disp('Dir too small to cut')
+        disp(horzcat('This dir was too small to cut: ', available_dirs(nrec).name))
     end
 end
