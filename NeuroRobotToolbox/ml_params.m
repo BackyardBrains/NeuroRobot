381d268a5ce80013e6eb655ab1d7a198179eb284
Christopher Harris <christopher@backyardbrains.com>
2023-12-23 17:53:43 -0500

    Update ml_params.m

--- NeuroRobotToolbox/ml_params.m
+++ NeuroRobotToolbox/ml_params.m
@@ -12,11 +12,11 @@ ntuples = numel(fullfile(strcat(dataset_dir_name, rec_dir_name), '**\*serial_dat
 disp(horzcat('ntuples: ', num2str(ntuples)))
 disp(horzcat('learn speed: ', num2str(learn_speed)))
 
-nsmall = round((0.001 * ndists + 1000) * learn_speed * 2);
-bof_branching = round((0.0003 * ndists + 200) * learn_speed);
-nmedium = round((0.005 * ndists + 3000) * learn_speed);
-init_n_unique_states = max([10 round(0.0005 * ndists * learn_speed) + 10 * learn_speed]);
-min_size = max([5 round((0.00015 * ndists * learn_speed) + 10 * learn_speed)]);
+nsmall = round((0.001 * ntuples + 1000) * learn_speed * 2);
+bof_branching = round((0.0003 * ntuples + 200) * learn_speed);
+nmedium = round((0.005 * ntuples + 3000) * learn_speed);
+init_n_unique_states = max([10 round(0.0005 * ntuples * learn_speed) + 10 * learn_speed]);
+min_size = max([5 round((0.00015 * ntuples * learn_speed) + 10 * learn_speed)]);
 
 disp(horzcat('nsmall = ', num2str(nsmall)))
 disp(horzcat('nmedium = ', num2str(nmedium)))
