977db086da093b4b82f1e70384cc2356c14f6dac
Christopher Harris <christopher@backyardbrains.com>
2023-12-28 11:27:20 -0500

    v4.1

--- NeuroRobotToolbox/neurorobot.m
+++ NeuroRobotToolbox/neurorobot.m
@@ -61,12 +61,12 @@ end
 %% Prepare figure
 fig_startup = figure(1);
 clf
-set(fig_startup, 'NumberTitle', 'off', 'Name', 'SpikerBot 4.0')
+set(fig_startup, 'NumberTitle', 'off', 'Name', 'SpikerBot 4.1')
 set(fig_startup, 'menubar', 'none', 'toolbar', 'none')
 set(fig_startup, 'position', startup_fig_pos, 'color', fig_bg_col)
 
 % Title
-text_title = uicontrol('Style', 'text', 'String', 'SpikerBot 4.0', 'units', 'normalized', 'position', [0.05 0.7 0.9 0.25], ...
+text_title = uicontrol('Style', 'text', 'String', 'SpikerBot 4.1', 'units', 'normalized', 'position', [0.05 0.7 0.9 0.25], ...
     'FontName', gui_font_name, 'backgroundcolor', fig_bg_col, 'fontsize', bfsize + 40, 'horizontalalignment', 'center', 'fontweight', gui_font_weight);
 
 
