d8839f62ce38f486876f1a3b80a0ef21a72d516c
Christopher Harris <christopher@backyardbrains.com>
2024-02-05 19:19:57 -0500

    .

--- NeuroRobotToolbox/train_rcnn.m
+++ NeuroRobotToolbox/train_rcnn.m
@@ -1,48 +1,31 @@
 
-
-%% Get base net
-if ~exist('vnet', 'var')
-    % anet = alexnet;
-    vnet = vgg19;
-end
-
+%% Prep and label
+% if ~exist('vnet', 'var')
+%     % anet = alexnet;
+%     vnet = vgg19;
+% end
 
 % %% Get data
 image_dir_name = 'C:\Users\chris\OneDrive\Documents\MATLAB\Selected\livingroomWebcam\';
-image_dir2_name = 'C:\Users\chris\OneDrive\Documents\MATLAB\Selected\livingroomWebcam2\';
 
-image_dir = dir(horzcat(image_dir2_name, '*.png'));
+image_dir = dir(horzcat(image_dir_name, '*.png'));
 nims = size(image_dir, 1);
 disp(horzcat('nims: ', num2str(nims)))
 
+imageLabeler(image_dir_name)
 
-%% Resize
-% for nim = 1:nims
-%     this_im = strcat(image_dir_name, '\',  image_dir(nim).name);
-%     im = imread(this_im);
-%     k = imresize(im, 0.5);
-%     newfilename = strcat(image_dir2_name, '\',  image_dir(nim).name);
-%     imwrite(k, newfilename, 'png');
-%     disp(num2str(nim))
-% end
-
-
-%% Label images manually
-% imageLabeler(image_dir2_name)
-
-
-%% Save
-% save('office_gTruth', 'office_gTruth')
 
 
 %% Train net
-load('office_gTruth')
+% save('livingroom_chris_gTruth', 'livingroom_chris_gTruth')
 
-trainingData = objectDetectorTrainingData(office_gTruth);
+load('livingroom_chris_gTruth')
+
+trainingData = objectDetectorTrainingData(livingroom_chris_gTruth);
 
 % Net
 layers = [
-    imageInputLayer([120 160 3])
+    imageInputLayer([227 302 3])
     convolution2dLayer(3, 16)
     reluLayer  
     convolution2dLayer(3, 8)
@@ -58,14 +41,11 @@ layers = [
     ];
 
 % try adam optimizer
-options = trainingOptions('sgdm', ...
-    'MiniBatchSize', 32, ...
-    'InitialLearnRate', 1e-5, ...
-    'MaxEpochs', 5, ...
-    'verbosefrequency', 5, ...
-    'plots', 'training-progress');
+options = trainingOptions('sgdm', 'MiniBatchSize', 32, ...
+    'InitialLearnRate', 1e-5, 'MaxEpochs', 5, ...
+    'verbosefrequency', 5, 'plots', 'training-progress');
 
-rcnn = trainRCNNObjectDetector(trainingData, layers, options);
+rcnn = trainFastRCNNObjectDetector(trainingData, layers, options);
 
 save(horzcat(nets_dir_name, 'rcnn'), 'rcnn')
 
