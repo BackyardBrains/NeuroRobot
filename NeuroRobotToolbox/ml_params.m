00dc6ab08ba40af54affa48303b9eac641d08872
Christopher Harris <christopher@backyardbrains.com>
2024-04-01 18:49:59 -0400

    v5.2

--- NeuroRobotToolbox/ml_params.m
+++ NeuroRobotToolbox/ml_params.m
@@ -5,7 +5,7 @@ if ml_speed_select.Value == 1 % Fast
     nsmall = 2000;
     bof_branching = 500;
     nmedium = 2000;
-    init_n_unique_states = 100;
+    init_n_unique_states = 800;
     min_size = 20;
 elseif ml_speed_select.Value == 2 % Medium
     nsmall = 4000;
@@ -17,8 +17,8 @@ elseif ml_speed_select.Value == 3 % Slow
     nsmall = 8000;
     bof_branching = 500;
     nmedium = 8000;
-    init_n_unique_states = 280;
-    min_size = 80;
+    init_n_unique_states = 1000;
+    min_size = 40;
 end
 
 disp(horzcat('nsmall = ', num2str(nsmall)))
