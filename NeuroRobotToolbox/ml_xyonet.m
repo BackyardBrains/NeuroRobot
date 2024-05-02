1a5b47c48d525ab451fdc47afda671b6c2fff0d8
Christopher Harris <christopher@backyardbrains.com>
2024-05-02 09:14:24 -0400

    Update ml_xyonet.m

--- NeuroRobotToolbox/ml_xyonet.m
+++ NeuroRobotToolbox/ml_xyonet.m
@@ -47,8 +47,50 @@ for ntuple = 1:ntuples
 
 end
 
+thetas = round(thetas);
+robot_xys = round(robot_xys);
+
+x = robot_xys(:,1);
+y = robot_xys(:,2);
+
+n1 = sum(x < 100 | x > 500);
+ns = randsample(100:500, n1, 1);
+x(x < 100 | x > 500) = ns;
+
+n1 = sum(y < 100 | y > 400);
+ns = randsample(100:400, n1, 1);
+y(y < 100 | y > 400) = ns;
+
+n1 = sum(thetas > 360);
+ns = randsample(360, n1, 1);
+thetas(thetas > 360) = ns;
 
 %%
+
+figure(3)
+clf
+
+subplot(3,2,1)
+histogram(x)
+title('x')
+
+subplot(3,2,2)
+histogram(y)
+title('y')
+
+subplot(3,2,3)
+histogram(thetas)
+title('o')
+
+%%
+% xyo_l1 = 4;
+% xyo_l2 = 4;
+% xyo_l3 = 4;
+% xyo_l4 = 10;
+% xyo_l5 = 10;
+% xyo_drop = 1; % LearnRateDropPeriod
+% xyo_maxeps = 2;
+% xyo_minbatch = 128;
 
 xyos = arrayDatastore([robot_xys(:,1) robot_xys(:,2) thetas]);
 training_data = combine(image_ds, xyos);
@@ -96,4 +138,35 @@ options = trainingOptions("adam", ...
 xyoNet = trainnet(training_data, layers, 'mse', options);
 
 save(strcat(nets_dir_name, net_name), 'xyoNet')
+
+load(strcat(nets_dir_name, net_name))
+
+%%
+xyo_net_vals = zeros(ntuples, 3);
+for ntuple = 1:ntuples
+    if ~rem(ntuple, round(ntuples/10))
+        disp(num2str(ntuple/ntuples))
+    end
+    im = readimage(image_ds, ntuple);
+    xyo_net_vals(ntuple, :) = predict(xyoNet, double(im));
+end
+
+%%
+
+figure(3)
+
+subplot(3,2,4)
+scatter(robot_xys(:,1), xyo_net_vals(:,1))
+axis tight
+title('X')
+
+subplot(3,2,5)
+scatter(robot_xys(:,2), xyo_net_vals(:,2))
+axis tight
+title('Y')
+
+subplot(3,2,6)
+scatter(thetas, xyo_net_vals(:,3))
+axis tight
+title('O')
 
